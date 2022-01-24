e8428cc4a033d4942b0fd9c05e2f2a1bce2862b5
horey <alexey.beley@gmail.com>
2022-01-24 16:30:25 +0200

    added output dir

--- deployer/horey/deployer/remote_deployer.py
+++ deployer/horey/deployer/remote_deployer.py
@@ -99,6 +99,7 @@ class RemoteDeployer:
 
                 logger.info(f"sftp: mkdir {deployment_target.remote_target_deployment_directory_path}")
                 sftp_client.mkdir(deployment_target.remote_target_deployment_directory_path, ignore_existing=True)
+                sftp_client.mkdir(os.path.join(deployment_target.remote_target_deployment_directory_path, "output"), ignore_existing=True)
 
                 logger.info(f"sftp: put_dir {deployment_target.remote_target_deployment_directory_path}")
                 sftp_client.put_dir(deployment_target.local_deployment_dir_path,
