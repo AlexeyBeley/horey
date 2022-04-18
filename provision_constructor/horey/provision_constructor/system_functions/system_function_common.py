6d4441dabd4ca492edce0985e7dc724251abc83d
horey <alexey.beley@gmail.com>
2022-04-18 19:40:51 +0300

    debug

--- provision_constructor/horey/provision_constructor/system_functions/system_function_common.py
+++ provision_constructor/horey/provision_constructor/system_functions/system_function_common.py
@@ -3,6 +3,8 @@ import shutil
 import datetime
 import uuid
 import os
+
+import time
 from horey.common_utils.actions_manager import ActionsManager
 from horey.replacement_engine.replacement_engine import ReplacementEngine
 import argparse
@@ -156,12 +158,39 @@ class SystemFunctionCommon:
 
     @staticmethod
     def check_systemd_service_status(service_name=None, min_uptime=None):
+        pdb.set_trace()
+        status_name = "active"
+        status_change_seconds_limit = 120
         # 'Active: active (running) since Tue 2022-04-12 18:32:04 GMT; 2h 33min ago'
         service_status_raw = SystemFunctionCommon.get_systemd_service_status(service_name)
         service_status = SystemFunctionCommon.extract_service_status_value(service_status_raw)
-        server_time, duration = SystemFunctionCommon.extract_service_status_times(service_status_raw)
+        server_time, seconds_duration = SystemFunctionCommon.extract_service_status_times(service_status_raw)
 
-        pdb.set_trace()
+        time_limit = datetime.datetime.now() + datetime.timedelta(seconds=status_change_seconds_limit)
+        while service_status != status_name and datetime.datetime.now() < time_limit:
+            time.sleep(5)
+            service_status_raw = SystemFunctionCommon.get_systemd_service_status(service_name)
+            service_status = SystemFunctionCommon.extract_service_status_value(service_status_raw)
+            server_time, seconds_duration = SystemFunctionCommon.extract_service_status_times(service_status_raw)
+
+        if service_status != status_name:
+            raise TimeoutError(f"service {service_name} did not reach {status_name} in {status_change_seconds_limit} seconds")
+
+        if min_uptime < seconds_duration:
+            return True
+
+        time.sleep(min_uptime-seconds_duration)
+        service_status_raw = SystemFunctionCommon.get_systemd_service_status(service_name)
+        service_status = SystemFunctionCommon.extract_service_status_value(service_status_raw)
+        server_time, seconds_duration = SystemFunctionCommon.extract_service_status_times(service_status_raw)
+        if service_status != status_name:
+            raise TimeoutError(f"service {service_name} seams to be in restart loop")
+
+        if min_uptime < seconds_duration:
+            return True
+
+        raise TimeoutError(f"service {service_name} seams to be in restart loop after cause it can not keep {status_name} "
+                           f"status for {min_uptime} seconds ")
 # endregion
 
     @staticmethod
@@ -192,10 +221,10 @@ class SystemFunctionCommon:
         str_time_data = " ".join(lst_line[since_index + 1:])
         start_date_str, duration_string = str_time_data.split("; ")
         start_date = datetime.datetime.strptime(start_date_str, "%a %Y-%m-%d %H:%M:%S GMT")
-        return start_date, SystemFunctionCommon.extract_service_status_time_duration(duration_string)
+        return start_date, SystemFunctionCommon.extract_service_status_seconds_duration(duration_string)
 
     @staticmethod
-    def extract_service_status_time_duration(duration_string):
+    def extract_service_status_seconds_duration(duration_string):
         """
         2h 33min ago'
         3 months 19 days ago
@@ -234,16 +263,16 @@ class SystemFunctionCommon:
 
         for duration_part in duration_lst[:-1]:
             if duration_part.endswith("s"):
-                seconds = duration_part[:-1]
+                seconds = int(duration_part[:-1])
             elif duration_part.endswith("h"):
-                hours = duration_part[:-1]
+                hours = int(duration_part[:-1])
             elif duration_part.endswith("min"):
-                minutes = duration_part[:-3]
+                minutes = int(duration_part[:-3])
             else:
                 raise ValueError(f"{duration_part} in {duration_lst}")
+
         days = days + 365*years + months*30
-        pdb.set_trace()
-        return datetime.timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds)
+        return days*24*60*60 + 60*60*hours + minutes*60 + seconds
 
     @staticmethod
     def extract_service_status_line_raw(service_status_raw):
