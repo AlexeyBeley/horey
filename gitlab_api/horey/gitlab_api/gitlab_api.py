1e24bda77884825185b74d814f011be0678988df
horey <alexey.beley@gmail.com>
2022-05-23 22:47:24 +0300

    added add user

--- gitlab_api/horey/gitlab_api/gitlab_api.py
+++ gitlab_api/horey/gitlab_api/gitlab_api.py
@@ -14,12 +14,16 @@ logger = get_logger()
 
 class GitlabAPI:
     def __init__(self, configuration: GitlabAPIConfigurationPolicy = None):
+        self.projects = []
         self.token = configuration.token
         self.group_id = configuration.group_id
         self.base_address = "https://gitlab.com"
 
     def get(self, request_path):
         request = self.create_request(request_path)
+        return self.get_raw(request)
+
+    def get_raw(self, request):
         headers = {}
 
         if self.token is not None:
@@ -37,6 +41,9 @@ class GitlabAPI:
 
     def post(self, request_path, data):
         request = self.create_request(request_path)
+        return self.post_raw(request, data)
+
+    def post_raw(self, request, data):
         headers = {"Content-Type": "application/json"}
 
         if self.token is not None:
@@ -46,7 +53,7 @@ class GitlabAPI:
             request, data=json.dumps(data),
             headers=headers)
 
-        if response.status_code != 200:
+        if response.status_code not in [200, 201]:
             raise RuntimeError(
                 f'Request to gitlab api returned an error {response.status_code}, the response is:\n{response.text}'
             )
@@ -63,33 +70,11 @@ class GitlabAPI:
             request,
             headers=headers
         )
-        if response.status_code != 200:
+        if response.status_code not in [200, 201]:
             raise RuntimeError(
                 f'Request to gitlab api returned an error {response.status_code}, the response is:\n{response.text}'
             )
         return response.json()
-
-    def provision_organisation(self, org_name):
-        logger.info(f"Provisioning organisation {org_name}")
-        response = self.get("/orgs")
-        for org in response:
-            if org["name"] == org_name:
-                logger.info(f"Found organisation: {response}")
-                return org["id"]
-
-        response = self.post("/orgs", {"name": org_name})
-        logger.info(f"Provisioned organisation: {response}")
-        return response["orgId"]
-
-    def add_user_to_organisation(self, org_id, user):
-        response = self.get(f"/orgs/{org_id}/users")
-        for org in response:
-            if org["orgId"] == org_id and org["login"] == user:
-                logger.info(f"Found user {user} in organisation: {response}")
-                return
-
-        response = self.post(f"/orgs/{org_id}/users", {"loginOrEmail": user, "role": "Admin"})
-        logger.info(f"Added user {user} to organisation: {response}")
 
     def create_request(self, request: str):
         """
@@ -101,12 +86,28 @@ class GitlabAPI:
         if request.startswith("/"):
             request = request[1:]
 
-        return f"{self.base_address}/api/v4/groups/{self.group_id}/{request}"
+        return f"{self.base_address}/api/v4/{request}"
 
     def init_projects(self):
-        for dict_src in self.get("projects"):
-            print(dict_src)
-            pdb.set_trace()
+        for dict_src in self.get(f"groups/{self.group_id}/projects"):
+            self.projects.append(dict_src)
+
+    def add_user_to_projects(self, projects, user_id):
+        for dict_src in projects:
+            ret = self.get_raw(dict_src["_links"]["members"])
+            if user_id in str(ret):
+                continue
+            #pdb.set_trace()
+            data = {"user_id": user_id, "access_level": 40}
+            try:
+                self.post(f"projects/{dict_src['id']}/members", data)
+                logger.info(f"Added to {dict_src['name']}")
+            except Exception as inst:
+                logger.error(f"{dict_src['name']} : {repr(inst)}")
+                if "An error 403" not in repr(inst):
+                    raise
+        pdb.set_trace()
+
 
     def provision_datasource(self, datasource):
         self.post("datasources", datasource.generate_create_request())
--- gitlab_api/tests/test_gitlab_api.py
+++ gitlab_api/tests/test_gitlab_api.py
@@ -26,7 +26,16 @@ def test_init_projects():
     gitlab_api.init_projects()
     assert isinstance(gitlab_api.projects, list)
 
+@pytest.mark.skip(reason="Can not test")
+def test_add_user_to_projects():
+    gitlab_api.init_projects()
+    user_id = ""
+    pdb.set_trace()
+    gitlab_api.add_user_to_projects(gitlab_api.projects, user_id=user_id)
+    assert isinstance(gitlab_api.projects, list)
+
 if __name__ == "__main__":
     #test_init_gitlab_api()
-    test_init_projects()
+    #test_init_projects()
+    test_add_user_to_projects()
     #test_provision_dashboard()
