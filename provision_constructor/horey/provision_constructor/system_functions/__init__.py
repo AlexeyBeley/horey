7b96e26059f30804ca628a8369048ff1e0adddc5
horey <alexey.beley@gmail.com>
2022-06-20 14:15:07 +0300

    debug

--- provision_constructor/horey/provision_constructor/system_functions/__init__.py
+++ provision_constructor/horey/provision_constructor/system_functions/__init__.py
@@ -8,4 +8,4 @@ from .logrotate import *
 from .ntp import *
 from .python import *
 from .java import *
-from .apt_package import *
+from .apt_package_generic import *
--- provision_constructor/tests/test_provision_constructor_ng.py
+++ provision_constructor/tests/test_provision_constructor_ng.py
@@ -24,7 +24,7 @@ def test_add_system_function_logstash():
     provision_constructor = ProvisionConstructor()
     #provision_constructor.provision_system_function("ntp")
     #provision_constructor.provision_system_function("java")
-    provision_constructor.provision_system_function("apt_package", package_name="curl")
+    provision_constructor.provision_system_function("apt_package_generic", package_name="curl")
     return
     provision_constructor.add_system_function("logrotate", force=True, rotation_path="/var/log/nginx", file_name="nginx")
     provision_constructor.add_system_function("logstash", force=True)
