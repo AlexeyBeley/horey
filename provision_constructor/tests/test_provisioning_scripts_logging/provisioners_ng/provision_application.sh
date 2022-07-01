71d2482d0221bd5fdf81d632d2059cb67f2390c9
horey <alexey.beley@gmail.com>
2022-07-01 21:34:39 +0300

    debug

--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision_application.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision_application.sh
@@ -2,4 +2,4 @@
 
 source logger.sh
 
-pip install flask==5.1.0 2> >(log_stdin_error) || traceback "Failed to install flask"
+pip install flask==5.1.0 1> >(log_stdin_info) 2> >(log_stdin_error) || traceback "Failed to install Flask"
--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision_gunicorn.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision_gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip install gunicorn
+pip install gunicorn 1> >(log_stdin_info) 2> >(log_stdin_error) || traceback "Failed to install Gunicorn"
