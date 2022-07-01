1c997f3008c80e9cd02e6315bbde5a34465e2482
horey <alexey.beley@gmail.com>
2022-07-01 21:30:11 +0300

    debug

--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/logger.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/logger.sh
@@ -21,13 +21,12 @@ function log_error() {
 }
 
 function traceback() {
-  #args="$*"
-  #echo "($(basename "${BASH_SOURCE[1]}"):${BASH_LINENO[0]}) [ERROR]: ${args}. Traceback:"
+  args="$*"
+  >&2 echo "($(basename "${BASH_SOURCE[1]}"):${BASH_LINENO[0]}) [ERROR]: ${args}. Traceback:"
 
   for (( idx=${#BASH_LINENO[@]}-1 ; idx>=0 ; idx-- )) ; do
-    echo "idx: ${idx} ${BASH_SOURCE[*]}"
     bash_line_number="${BASH_LINENO[idx]}"
-    echo "$(get_abs_filename ${BASH_SOURCE[$((idx + 1))]}):${bash_line_number}"
+    >&2 echo "$(get_abs_filename ${BASH_SOURCE[$((idx + 1))]}):${bash_line_number}"
   done
   exit 1
 }
--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision.sh
@@ -8,6 +8,6 @@ set +x
 
 source logger.sh
 
-./provision_application.sh 1> >(log_stdin_info)
 ./provision_python39.sh 1> >(log_stdin_info)
 ./provision_gunicorn.sh 1> >(log_stdin_info)
+./provision_application.sh 1> >(log_stdin_info)
