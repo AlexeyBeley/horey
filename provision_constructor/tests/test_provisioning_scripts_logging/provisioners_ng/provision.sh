3932609675480b22cc6eda2a9ca66964c7bc4082
horey <alexey.beley@gmail.com>
2022-07-01 18:13:17 +0300

    debug

--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${SCRIPT_DIR}"
+
 set -e
 set +x
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "${SCRIPT_DIR}"
+source logger.sh
 
 ./provision_python39.sh
 ./provision_gunicorn.sh
