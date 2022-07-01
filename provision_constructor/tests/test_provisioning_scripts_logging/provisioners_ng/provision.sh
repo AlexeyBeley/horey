2ee4ba55ffe4eb68a7adf479a7a46f97fb981743
horey <alexey.beley@gmail.com>
2022-07-01 21:20:39 +0300

    debug

--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision.sh
@@ -8,6 +8,6 @@ set +x
 
 source logger.sh
 
-./provision_application.sh 1> >(log_stdin_info) || traceback "Failed to run provision_application.sh"
-./provision_python39.sh 1> >(log_stdin_info) || traceback "Failed to run provision_python39.sh"
-./provision_gunicorn.sh 1> >(log_stdin_info) || traceback "Failed to run provision_gunicorn.sh"
+./provision_application.sh 1> >(log_stdin_info)
+./provision_python39.sh 1> >(log_stdin_info)
+./provision_gunicorn.sh 1> >(log_stdin_info)
