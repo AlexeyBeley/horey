e8e9e15c806924ff94f56071c71bcca84b7435fd
horey <alexey.beley@gmail.com>
2022-07-01 21:42:17 +0300

    debug

--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/retry.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/retry.sh
@@ -19,7 +19,7 @@ function unlock_frontend_file () {
   PID=$(lsof /var/lib/dpkg/lock-frontend | awk '/[0-9]+/{print $2}')
   if [ -n "${PID}" ]
   then
-	    log_info "Killing process holding dpkg/lock-frontend : ${PID}"
+	    #log_info "Killing process holding dpkg/lock-frontend : ${PID}"
       kill -s 9 "${PID}" 2> >(log_stdin_error) || true
   fi
 }
@@ -30,8 +30,8 @@ set +e
 for VARIABLE in {1..2}
 do
   unlock_frontend_file
-	log_info "Running: '$*'"
-  "$@" 2> >(log_stdin_error)
+	#log_info "Running: '$*'"
+  "$@" #2> >(log_stdin_error)
   return_code=$?
   if [ "$return_code" == 0 ]
   then
@@ -40,7 +40,7 @@ do
   fi
   log_info "Retry ${VARIABLE}/10 going to sleep for 5 seconds"
   sleep 5
-traceback "Timeout reached while executing '$*'"
+#traceback "Timeout reached while executing '$*'"
 exit 1
 done
 
