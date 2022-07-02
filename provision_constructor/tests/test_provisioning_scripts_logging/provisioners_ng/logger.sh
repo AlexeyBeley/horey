b2c4b8f81fd3c5f6bbb0d3a72f88e9993870ab5d
horey <alexey.beley@gmail.com>
2022-07-02 11:31:36 +0300

    debug

--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/logger.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/logger.sh
@@ -52,6 +52,5 @@ function log_stdin_info() {
 }
 
 function logged(){
-   "$@" 1> >(log_stdin_info) 2> >(log_stdin_error) || traceback "Failed to install Flask"
-
+   "$@" 1> >(log_stdin_info) 2> >(log_stdin_error) || traceback "Failed execute $*"
 }
