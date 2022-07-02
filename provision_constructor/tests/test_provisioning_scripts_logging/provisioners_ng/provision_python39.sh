6b887aa4e50005dfd180c96c6e544902beedfbff
horey <alexey.beley@gmail.com>
2022-07-02 11:04:06 +0300

    debug

--- provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision_python39.sh
+++ provision_constructor/tests/test_provisioning_scripts_logging/provisioners_ng/provision_python39.sh
@@ -11,8 +11,8 @@ logged retry_10_times_sleep_5 add-apt-repository -y ppa:deadsnakes/ppa
 logged retry_10_times_sleep_5 apt-get purge --auto-remove -y python3.6
 
 logged retry_10_times_sleep_5 apt-get install -yqq python3.9 python3.9-distutils python3.9-dev python3.9-testsuite python3.9-stdlib
-ln -s /usr/bin/python3.9 /usr/bin/python
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py 1> >(log_stdin_info) 2> >(log_stdin_error) || traceback "Failed to install Gunicorn"
+logged ln -s /usr/bin/python3.9 /usr/bin/python
+logged curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+logged python get-pip.py
 
-rm get-pip.py
+logged rm get-pip.py
