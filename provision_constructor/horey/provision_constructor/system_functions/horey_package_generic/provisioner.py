a1f98adfbb5060692ea0fad20b32aec28002c5f9
horey <alexey.beley@gmail.com>
2022-08-15 22:05:24 +0300

    logs

--- provision_constructor/horey/provision_constructor/system_functions/horey_package_generic/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/horey_package_generic/provisioner.py
@@ -1,5 +1,9 @@
+"""
+Provision horey packages from source.
+
+"""
+
 import os.path
-import pdb
 from horey.provision_constructor.system_function_factory import SystemFunctionFactory
 
 from horey.provision_constructor.system_functions.system_function_common import SystemFunctionCommon
@@ -10,6 +14,11 @@ logger = get_logger()
 
 @SystemFunctionFactory.register
 class Provisioner(SystemFunctionCommon):
+    """
+    Provisioner class.
+
+    """
+
     def __init__(self, deployment_dir, horey_repo_path=None, package_name=None, package_names=None):
         super().__init__(os.path.dirname(os.path.abspath(__file__)))
         self.deployment_dir = deployment_dir
@@ -24,17 +33,39 @@ class Provisioner(SystemFunctionCommon):
         self.horey_repo_path = horey_repo_path
 
     def provision(self, force=False):
+        """
+        Provision all packages.
+
+        @param force:
+        @return:
+        """
+
         for package_name in self.package_names:
             if not force and self.test_provisioned(package_name):
+                logger.info(f"Skipping horey package installation: {package_name}")
                 continue
 
             self._provision(package_name)
 
             self.test_provisioned(package_name)
+            logger.info(f"Successfully installed horey package: {package_name}")
 
     def test_provisioned(self, package_name):
+        """
+        Test single package provisioned.
+
+        @param package_name:
+        @return:
+        """
+
         return self.check_pip_installed(f"horey.{package_name}")
 
     def _provision(self, package_name):
-        self.run_bash(f"cd {self.horey_repo_path} && make recursive_install_from_source-{package_name}")
+        """
+        Provision single packages.
 
+        @param package_name:
+        @return:
+        """
+
+        self.run_bash(f"cd {self.horey_repo_path} && make recursive_install_from_source-{package_name}")
