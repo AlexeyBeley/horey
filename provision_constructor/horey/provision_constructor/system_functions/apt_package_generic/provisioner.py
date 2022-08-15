0fbaeec881788d1daeca866a8d81b8ae870617bf
horey <alexey.beley@gmail.com>
2022-08-15 22:36:07 +0300

    logs

--- provision_constructor/horey/provision_constructor/system_functions/apt_package_generic/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/apt_package_generic/provisioner.py
@@ -1,5 +1,9 @@
+"""
+APT packages provisioning.
+
+"""
+
 import os.path
-import pdb
 from horey.provision_constructor.system_function_factory import SystemFunctionFactory
 
 from horey.provision_constructor.system_functions.system_function_common import SystemFunctionCommon
@@ -10,23 +14,57 @@ logger = get_logger()
 
 @SystemFunctionFactory.register
 class Provisioner(SystemFunctionCommon):
-    def __init__(self, deployment_dir, package_name=None):
+    """
+    Main provisioner.
+
+    """
+
+    def __init__(self, deployment_dir, package_name=None, package_names=None):
         super().__init__(os.path.dirname(os.path.abspath(__file__)))
         self.deployment_dir = deployment_dir
-        self.package_name = package_name
+
+        if package_name is not None:
+            self.package_names = [package_name]
+        elif package_names is not None:
+            self.package_names = package_names
+        else:
+            raise ValueError("package_name/package_names not set")
 
     def provision(self, force=False):
-        if not force:
-            if self.test_provisioned():
-                return
+        """
+        Provision all packages.
 
-        self._provision()
+        @param force:
+        @return:
+        """
 
-        self.test_provisioned()
+        for package_name in self.package_names:
+            if not force and self.test_provisioned(package_name):
+                logger.info(f"Skipping apt package installation: {package_name}")
+                continue
 
-    def test_provisioned(self):
+            self._provision(package_name)
+
+            self.test_provisioned(package_name)
+            logger.info(f"Successfully installed apt package: {package_name}")
+
+    def test_provisioned(self, package_name):
+        """
+        Test single package provisioned.
+
+        @param package_name:
+        @return:
+        """
+
         self.init_apt_packages()
-        return self.apt_check_installed(self.package_name)
+        return self.apt_check_installed(package_name)
 
-    def _provision(self):
-        return self.apt_install(self.package_name)
+    def _provision(self, package_name):
+        """
+        Provision single packages.
+
+        @param package_name:
+        @return:
+        """
+
+        return self.apt_install(package_name)
