0f3d26d040239998f28b51bba1942448cf5f383a
horey <alexey.beley@gmail.com>
2022-08-24 16:55:23 +0300

    Added default

--- aws_api/horey/aws_api/aws_services_entities/event_bridge_target.py
+++ aws_api/horey/aws_api/aws_services_entities/event_bridge_target.py
@@ -2,8 +2,6 @@
 Event bridge target representation
 """
 
-import pdb
-
 from horey.aws_api.aws_services_entities.aws_object import AwsObject
 from horey.aws_api.base_entities.region import Region
 
@@ -18,6 +16,8 @@ class EventBridgeTarget(AwsObject):
         self._region = None
         self.role_arn = None
         self.input = None
+        self.arn = None
+        self.ecs_parameters = None
 
         if from_cache:
             self._init_object_from_cache(dict_src)
@@ -43,6 +43,12 @@ class EventBridgeTarget(AwsObject):
         self._init_from_cache(dict_src, options)
 
     def update_from_raw_response(self, dict_src):
+        """
+        Standard
+
+        @param dict_src:
+        @return:
+        """
         init_options = {
             "Id": self.init_default_attr,
             "Arn": self.init_default_attr,
@@ -54,18 +60,31 @@ class EventBridgeTarget(AwsObject):
         self.init_attrs(dict_src, init_options)
 
     def generate_put_request(self):
-        request = dict()
-        request["Id"] = self.id
-        request["Arn"] = self.arn
+        """
+        Standard
+
+        @return:
+        """
+        request = {"Id": self.id,
+                   "Arn": self.arn}
         if self.role_arn is not None:
             request["RoleArn"] = self.role_arn
+
         if self.input is not None:
             request["Input"] = self.input
+
+        if self.ecs_parameters is not None:
+            request["EcsParameters"] = self.ecs_parameters
 
         return request
 
     @property
     def region(self):
+        """
+        Standard
+
+        @return:
+        """
         if self._region is not None:
             return self._region
 
@@ -76,6 +95,12 @@ class EventBridgeTarget(AwsObject):
 
     @region.setter
     def region(self, value):
+        """
+        Standard
+
+        @param value:
+        @return:
+        """
         if not isinstance(value, Region):
             raise ValueError(value)
 
