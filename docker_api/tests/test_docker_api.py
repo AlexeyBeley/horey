33167e9d3238b63e82b7cc64e22686c343cb5b61
horey <alexey.beley@gmail.com>
2022-09-04 20:29:17 +0300

    added ecr add tag

--- docker_api/tests/test_docker_api.py
+++ docker_api/tests/test_docker_api.py
@@ -15,9 +15,6 @@ mock_values = CommonUtils.load_object_from_module(mock_values_file_path, "main")
 src_aws_region = "us-west-2"
 dst_aws_region = "us-west-2"
 
-src_aws_region = "us-east-1"
-
-
 def test_init_docker_api():
     """
     Test basic init.
