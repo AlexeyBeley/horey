89b5c979e7cce8e50434754e0f7af49682a504d8
horey <alexey.beley@gmail.com>
2022-10-15 15:36:22 +0300

    Debug

--- gitlab_api/horey/gitlab_api/jenkins_authorization/authorization_actor.py
+++ gitlab_api/horey/gitlab_api/jenkins_authorization/authorization_actor.py
@@ -10,5 +10,10 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--jenkins_job_json", type=str)
+    parser.add_argument("--identity", type=str)
     configuration_args = parser.parse_args()
-    print(json.loads(configuration_args.jenkins_job_json))
+    try:
+        print(json.loads(configuration_args.jenkins_job_json))
+    except Exception as error_instance:
+        print(f"Error: {error_instance}")
+    print(configuration_args.identity)
