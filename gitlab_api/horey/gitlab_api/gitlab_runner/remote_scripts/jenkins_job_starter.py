10da6e9e79a0d1fd749083b820051627a406a521
horey <alexey.beley@gmail.com>
2022-10-26 00:20:42 +0300

    debug

--- gitlab_api/horey/gitlab_api/gitlab_runner/remote_scripts/jenkins_job_starter.py
+++ gitlab_api/horey/gitlab_api/gitlab_runner/remote_scripts/jenkins_job_starter.py
@@ -8,7 +8,8 @@ Jenkins job being authorized and started.
 #['/opt/jenkins_jobs_starter', '/usr/lib/python38.zip', '/usr/lib/python3.8', '/usr/lib/python3.8/lib-dynload', '/usr/local/lib/python3.8/dist-packages', '/usr/lib/python3/dist-packages']
 
 #python3.8 /opt/jenkins_jobs_starter/jenkins_job_starter.py --jenkins_job_json "{\"job_name\":\"1\", \"arguments\":{\"arg_1\":\"val_1\"}, \"identity\":[\"${CI_COMMIT_AUTHOR}\"]}"
- - source /opt/jenkins_jobs_starter/venv/bin/activate && python3.8 /opt/jenkins_jobs_starter/jenkins_job_starter.py --job_name "name" --parameters "{\"arg_1\":\"val_1\"}" --user_identity "{\"CI_COMMIT_AUTHOR\": \"${CI_COMMIT_AUTHOR}\"}"
+#python3.8 /opt/jenkins_jobs_starter/jenkins_job_starter.py --job_name "{\"job_name\":\"1\", \"arguments\":{\"arg_1\":\"val_1\"}, \"identity\":[\"${CI_COMMIT_AUTHOR}\"]}"
+ - source /opt/jenkins_jobs_starter/venv/bin/activate && python3.8 /opt/jenkins_jobs_starter/jenkins_job_starter.py --job_name "jobname" --user_identity "{\"CI_COMMIT_AUTHOR\":\"${CI_COMMIT_AUTHOR}\"}" --parameters "{\"arg_1\":\"val_1\"}"
 """
 
 import json
@@ -25,7 +26,6 @@ if __name__ == "__main__":
     parser.add_argument("--parameters", type=str)
     parser.add_argument("--user_identity", type=str)
     configuration_args = parser.parse_args()
-    print(json.loads(configuration_args.jenkins_job_json))
     configuration = JenkinsConfigurationPolicy()
     configuration.configuration_file_full_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                                               "jenkins_manager_configuration.py")
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     jenkins_manager = JenkinsManager(configuration)
     request = {"job_name": configuration_args.job_name,
                "user_identity": configuration_args.user_identity,
-               "parameters": configuration_args.parameters}
+               "parameters": json.loads(configuration_args.parameters)}
 
-    report = jenkins_manager.execute_jobs([JenkinsJob("authenticator", json.dumps({"request": request}),
+    report = jenkins_manager.execute_jobs([JenkinsJob("authenticator", {"request": json.dumps(request)},
                                                       uid_parameter_name="data")])
     print(report)
