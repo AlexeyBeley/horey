f6c27b8600dfe987309662375f3526f5cd2496ed
horey <alexey.beley@gmail.com>
2023-02-20 09:12:42 +0200

    Refactor ECR image

--- aws_api/horey/aws_api/aws_services_entities/ecr_image.py
+++ aws_api/horey/aws_api/aws_services_entities/ecr_image.py
@@ -1,10 +1,8 @@
 """
 AWS Lambda representation
 """
-import pdb
 
 from horey.aws_api.aws_services_entities.aws_object import AwsObject
-from horey.aws_api.base_entities.region import Region
 
 
 class ECRImage(AwsObject):
@@ -32,6 +30,7 @@ class ECRImage(AwsObject):
             "imageScanFindingsSummary": self.init_default_attr,
             "imageManifestMediaType": self.init_default_attr,
             "artifactMediaType": self.init_default_attr,
+            "lastRecordedPullTime": self.init_default_attr,
         }
 
         self.init_attrs(dict_src, init_options)
@@ -44,76 +43,3 @@ class ECRImage(AwsObject):
         """
         options = {}
         self._init_from_cache(dict_src, options)
-
-    def update_value_from_raw_response(self, raw_value):
-        raise NotImplementedError()
-        pdb.set_trace()
-
-    def generate_create_request(self):
-        raise NotImplementedError()
-        """
-        TagSpecifications=[
-        {
-            'ResourceType': 'client-vpn-endpoint'|'customer-gateway'|'dedicated-host'|'dhcp-options'|'egress-only-internet-gateway'|'elastic-ip'|'elastic-gpu'|'export-image-task'|'export-instance-task'|'fleet'|'fpga-image'|'host-reservation'|'image'|'import-image-task'|'import-snapshot-task'|'instance'|'internet-gateway'|'key-pair'|'launch-template'|'local-gateway-route-table-vpc-association'|'natgateway'|'network-acl'|'network-interface'|'network-insights-analysis'|'network-insights-path'|'placement-group'|'reserved-instances'|'route-table'|'security-group'|'snapshot'|'spot-fleet-request'|'spot-instances-request'|'subnet'|'traffic-mirror-filter'|'traffic-mirror-session'|'traffic-mirror-target'|'transit-gateway'|'transit-gateway-attachment'|'transit-gateway-connect-peer'|'transit-gateway-multicast-domain'|'transit-gateway-route-table'|'volume'|'vpc'|'vpc-peering-connection'|'vpn-connection'|'vpn-gateway'|'vpc-flow-log',
-            'Tags': [
-                {
-                    'Key': 'string',
-                    'Value': 'string'
-                },
-            ]
-        },
-    ],
-       """
-        request = dict()
-        request["CidrBlock"] = self.cidr_block
-        request["AvailabilityZoneId"] = self.availability_zone_id
-        request["VpcId"] = self.vpc_id
-        request["TagSpecifications"] = [{"ResourceType": "subnet", "Tags": self.tags}]
-
-        return request
-
-    def update_from_raw_create(self, dict_src):
-        raise NotImplementedError()
-        pdb.set_trace()
-        init_options = {
-            "ECRImageId": lambda x, y: self.init_default_attr(
-                x, y, formatted_name="id"
-            ),
-            "ECRImageArn": lambda x, y: self.init_default_attr(
-                x, y, formatted_name="arn"
-            ),
-            "AvailabilityZone": self.init_default_attr,
-            "AvailabilityZoneId": self.init_default_attr,
-            "AvailableIpAddressCount": self.init_default_attr,
-            "CidrBlock": self.init_default_attr,
-            "DefaultForAz": self.init_default_attr,
-            "MapPublicIpOnLaunch": self.init_default_attr,
-            "MapCustomerOwnedIpOnLaunch": self.init_default_attr,
-            "State": self.init_default_attr,
-            "VpcId": self.init_default_attr,
-            "OwnerId": self.init_default_attr,
-            "AssignIpv6AddressOnCreation": self.init_default_attr,
-            "Ipv6CidrBlockAssociationSet": self.init_default_attr,
-            "Tags": self.init_default_attr,
-        }
-
-        self.init_attrs(dict_src, init_options)
-
-    @property
-    def region(self):
-        raise NotImplementedError()
-        if self._region is not None:
-            return self._region
-
-        if self.arn is not None:
-            self._region = Region.get_region(self.arn.split(":")[3])
-
-        return self._region
-
-    @region.setter
-    def region(self, value):
-        raise NotImplementedError()
-        if not isinstance(value, Region):
-            raise ValueError(value)
-
-        self._region = value
