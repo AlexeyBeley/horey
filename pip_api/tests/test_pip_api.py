2648a244fa408dada7e4886684e1320d0df61059
horey <alexey.beley@gmail.com>
2023-03-22 10:23:58 +0200

    Refactor pip_api

--- pip_api/tests/test_pip_api.py
+++ pip_api/tests/test_pip_api.py
@@ -5,7 +5,7 @@ Pip API tests
 
 import os
 from horey.common_utils.common_utils import CommonUtils
-from horey.pip_api.pip_api import PipAPI
+from horey.pip_api.pip_api import PipAPI, Requirement
 from horey.pip_api.pip_api_configuration_policy import PipAPIConfigurationPolicy
 
 
@@ -20,6 +20,16 @@ mock_values = CommonUtils.load_object_from_module(mock_values_file_path, "main")
 def test_init():
     pip_api = PipAPI()
     assert isinstance(pip_api, PipAPI)
+
+
+def test_init_configuration():
+    pip_api_configuration_file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "pip_api_configuration.py"
+    )
+    configuration = PipAPIConfigurationPolicy()
+    configuration.configuration_file_full_path = pip_api_configuration_file_path
+    configuration.init_from_file()
+    PipAPI(configuration=configuration)
 
 
 def test_init_packages():
@@ -71,8 +81,70 @@ def test_install_requirements_real_data():
     assert isinstance(pip_api.packages, list)
 
 
+def test_update_existing_requirement_no_max():
+    pip_api = PipAPI()
+    requirement_this = Requirement("some/path", "requests>=2.28.1")
+    pip_api.REQUIREMENTS = {"requests": requirement_this}
+    requirement_other = Requirement("some/path", "requests>=2.28.2")
+    pip_api.update_existing_requirement(requirement_other)
+    assert pip_api.REQUIREMENTS["requests"].min_version == "2.28.2"
+    assert pip_api.REQUIREMENTS["requests"].max_version is None
+    assert pip_api.REQUIREMENTS["requests"].include_min
+    assert pip_api.REQUIREMENTS["requests"].include_max is None
+
+
+def test_update_existing_requirement_with_max():
+    pip_api = PipAPI()
+    requirement_this = Requirement("some/path", "requests>=2.28.1")
+    requirement_this.max_version = "2.29.1"
+    requirement_this.include_max = True
+    pip_api.REQUIREMENTS = {"requests": requirement_this}
+    requirement_other = Requirement("some/path", "requests>=2.28.2")
+    requirement_other.max_version = "2.29.1"
+    requirement_other.include_max = False
+    pip_api.update_existing_requirement(requirement_other)
+    assert pip_api.REQUIREMENTS["requests"].min_version == "2.28.2"
+    assert pip_api.REQUIREMENTS["requests"].max_version == "2.29.1"
+    assert pip_api.REQUIREMENTS["requests"].include_min
+    assert not pip_api.REQUIREMENTS["requests"].include_max
+
+
+def test_update_existing_requirement_single_version():
+    pip_api = PipAPI()
+    requirement_this = Requirement("some/path", "requests>=2.28.1")
+    requirement_this.max_version = "2.29.1"
+    requirement_this.include_max = True
+    pip_api.REQUIREMENTS = {"requests": requirement_this}
+    requirement_other = Requirement("some/path", "requests>=2.29.1")
+    pip_api.update_existing_requirement(requirement_other)
+    assert pip_api.REQUIREMENTS["requests"].min_version == "2.29.1"
+    assert pip_api.REQUIREMENTS["requests"].max_version == "2.29.1"
+    assert pip_api.REQUIREMENTS["requests"].include_min
+    assert pip_api.REQUIREMENTS["requests"].include_max
+
+
+def test_update_existing_requirement_exception_1():
+    pip_api = PipAPI()
+    requirement_this = Requirement("some/path", "requests>=2.28.1")
+    requirement_this.max_version = "2.29.1"
+    requirement_this.include_max = True
+    pip_api.REQUIREMENTS = {"requests": requirement_this}
+
+    requirement_other = Requirement("some/path", "requests>2.29.1")
+    pip_api.update_existing_requirement(requirement_other)
+    assert pip_api.REQUIREMENTS["requests"].min_version == "2.29.1"
+    assert pip_api.REQUIREMENTS["requests"].max_version == "2.29.1"
+    assert pip_api.REQUIREMENTS["requests"].include_min
+    assert pip_api.REQUIREMENTS["requests"].include_max
+
+
 if __name__ == "__main__":
-    test_init()
-    test_init_packages()
-    test_install_requirements()
-    test_install_requirements_real_data()
+    #test_init()
+    test_init_configuration()
+    #test_init_packages()
+    #test_install_requirements()
+    #test_install_requirements_real_data()
+    test_update_existing_requirement_no_max()
+    test_update_existing_requirement_with_max()
+    test_update_existing_requirement_single_version()
+    test_update_existing_requirement_exception_1()
