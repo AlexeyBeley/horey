a21387c8153feb0ab5397ce547c5c57fcc59ae4a
horey <alexey.beley@gmail.com>
2023-04-05 12:33:51 +0300

    Added debug

--- provision_constructor/horey/provision_constructor/system_functions/java/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/java/provisioner.py
@@ -12,24 +12,17 @@ logger = get_logger()
 
 @SystemFunctionFactory.register
 class Provisioner(SystemFunctionCommon):
-    def __init__(self, deployment_dir):
-        super().__init__(os.path.dirname(os.path.abspath(__file__)))
+    def __init__(self, deployment_dir, force, upgrade):
+        super().__init__(os.path.dirname(os.path.abspath(__file__)), force, upgrade)
         self.deployment_dir = deployment_dir
 
-    def provision(self, force=False):
-        if not force:
-            if self.test_provisioned():
-                return
-
-        self._provision()
-
-        self.test_provisioned()
-
     def test_provisioned(self):
+        breakpoint()
         self.init_apt_packages()
         return self.apt_check_installed("openjdk-11-jdk")
 
     def _provision(self):
+        breakpoint()
         if not self.apt_check_repository_exists("openjdk-r"):
             self.apt_add_repository("ppa:openjdk-r/ppa")
 
--- provision_constructor/horey/provision_constructor/system_functions/logstash/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/logstash/provisioner.py
@@ -24,6 +24,16 @@ class Provisioner(SystemFunctionCommon):
     def _provision(self):
         """
         Provision all components.
+        wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
+
+        retry_10_times_sleep_5 apt install --upgrade default-jre -y
+        retry_10_times_sleep_5 apt install --upgrade default-jdk -y
+        echo "deb [signed-by=/usr/share/keyrings/elastic-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
+        sudo rm  /etc/apt/sources.list.d/elastic-7.x.list
+        sudo apt update
+        sudo apt install --upgrade logstash -y
+        sudo systemctl daemon-reload
+        sudo systemctl enable logstash
 
         :return:
         """
@@ -33,6 +43,8 @@ class Provisioner(SystemFunctionCommon):
         SystemFunctionFactory.REGISTERED_FUNCTIONS["gpg_key"](self.deployment_dir, self.force,
                                                               self.upgrade, src_url=src_url,
                                                               dst_file_path=dst_file_path).provision()
+
+        SystemFunctionFactory.REGISTERED_FUNCTIONS["java"](self.deployment_dir, self.force, self.upgrade)
 
     def test_provisioned(self):
         """
