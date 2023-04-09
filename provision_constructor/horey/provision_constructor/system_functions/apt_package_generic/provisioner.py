f79c6523b2e9a78bc26aa162b1a1be1a1e96c6be
horey <alexey.beley@gmail.com>
2023-04-09 10:50:17 +0300

    Added debug

--- provision_constructor/horey/provision_constructor/system_functions/apt_package_generic/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/apt_package_generic/provisioner.py
@@ -21,8 +21,9 @@ class Provisioner(SystemFunctionCommon):
 
     """
 
-    def __init__(self, deployment_dir, package_name=None, package_names=None):
-        super().__init__(os.path.dirname(os.path.abspath(__file__)))
+    # pylint: disable= too-many-arguments
+    def __init__(self, deployment_dir, force, upgrade, package_name=None, package_names=None):
+        super().__init__(os.path.dirname(os.path.abspath(__file__)), force, upgrade)
         self.deployment_dir = deployment_dir
 
         if package_name is not None:
@@ -32,43 +33,23 @@ class Provisioner(SystemFunctionCommon):
         else:
             raise ValueError("package_name/package_names not set")
 
-    def provision(self, force=False):
+    def test_provisioned(self):
         """
-        Provision all packages.
+        Test all packages provisioned.
 
-        @param force:
-        @return:
-        """
-
-        for package_name in self.package_names:
-            if not force and self.test_provisioned(package_name):
-                logger.info(f"Skipping apt package installation: {package_name}")
-                continue
-
-            self._provision(package_name)
-
-            if not self.test_provisioned(package_name):
-                raise RuntimeError(f"Failed to install apt package '{package_name}'")
-
-            logger.info(f"Successfully installed apt package: {package_name}")
-
-    def test_provisioned(self, package_name):
-        """
-        Test single package provisioned.
-
-        @param package_name:
         @return:
         """
 
         self.init_apt_packages()
-        return self.apt_check_installed(package_name)
+        return all(self.apt_check_installed(package_name)
+                   for package_name in self.package_names)
 
-    def _provision(self, package_name):
+    def _provision(self):
         """
-        Provision single packages.
+        Provision packages.
 
-        @param package_name:
         @return:
         """
 
-        return self.apt_install(package_name)
+        for package_name in self.package_names:
+            self.apt_install(package_name)
