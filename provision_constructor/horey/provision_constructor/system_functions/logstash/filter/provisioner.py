17713b4056684791937107239d2d6e5763fac982
horey <alexey.beley@gmail.com>
2023-04-09 08:46:48 +0300

    Added debug

--- provision_constructor/horey/provision_constructor/system_functions/logstash/filter/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/logstash/filter/provisioner.py
@@ -1,10 +1,33 @@
-from system_function_common import SystemFunctionCommon
+"""
+Logstash filter provisioner.
+
+"""
+
+import os
+from horey.provision_constructor.system_function_factory import SystemFunctionFactory
+from horey.provision_constructor.system_functions.system_function_common import (
+    SystemFunctionCommon,
+)
 
 
+@SystemFunctionFactory.register
 class Provisioner(SystemFunctionCommon):
-    def __init__(self):
-        super().__init__()
+    """
+    Main class.
 
+    """
 
-if __name__ == "__main__":
-    Provisioner.ACTION_MANAGER.call_action()
+    def __init__(self, deployment_dir, force, upgrade):
+        super().__init__(os.path.dirname(os.path.abspath(__file__)), force, upgrade)
+        self.deployment_dir = deployment_dir
+
+    def _provision(self):
+        breakpoint()
+
+    def test_provisioned(self):
+        """
+        Test the sys_function was provisioned.
+
+        :return:
+        """
+        return self.apt_check_installed("logstash")
