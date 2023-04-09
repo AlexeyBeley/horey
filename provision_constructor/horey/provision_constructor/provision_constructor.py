41e5986efe650f8af0b13e60fa70b0b2a8a55b61
horey <alexey.beley@gmail.com>
2023-04-09 13:01:13 +0300

    removed debug

--- provision_constructor/horey/provision_constructor/provision_constructor.py
+++ provision_constructor/horey/provision_constructor/provision_constructor.py
@@ -104,7 +104,6 @@ class ProvisionConstructor:
                 "sudo rm -rf horey\n"
                 "git clone https://github.com/AlexeyBeley/horey.git\n"
                 "cd horey\n"
-                "git checkout --track origin/provision-logstash\n"
                 "make recursive_install_from_source-provision_constructor\n"
                 "cd .."
             )
