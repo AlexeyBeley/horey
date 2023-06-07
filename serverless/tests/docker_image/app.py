4fd908dff7978269d485cb7b81eb8fb5e9d5c244
horey <alexey.beley@gmail.com>
2023-06-07 19:03:54 +0300

    pep8

--- serverless/tests/docker_image/app.py
+++ serverless/tests/docker_image/app.py
@@ -1,5 +1,16 @@
+"""
+Echo function
+
+"""
 
 
 def handler(event, context):
+    """
+    Entrypoint
+
+    :param event:
+    :param context:
+    :return:
+    """
     print("Done")
     return {"statusCode": 200, "body": str(event) + str(context)}
