7c1c2ca522758682217c8cd6371c98a413896da1
horey <alexey.beley@gmail.com>
2023-09-15 12:54:41 +0300

    Added sqs

--- aws_api/tests/test_sqs_client.py
+++ aws_api/tests/test_sqs_client.py
@@ -1,33 +1,26 @@
+"""
+Test sqs client.
+
+"""
+
 import os
-import pdb
+import pytest
 
 from horey.aws_api.aws_clients.sqs_client import SQSClient
 from horey.aws_api.aws_services_entities.sqs_queue import SQSQueue
 
-from horey.h_logger import get_logger
 from horey.aws_api.base_entities.aws_account import AWSAccount
+from horey.aws_api.base_entities.region import Region
 from horey.common_utils.common_utils import CommonUtils
 
-
-configuration_values_file_full_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "h_logger_configuration_values.py"
-)
-logger = get_logger(
-    configuration_values_file_full_path=configuration_values_file_full_path
-)
-
-accounts_file_full_path = os.path.abspath(
-    os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        "..",
-        "ignore",
-        "aws_api_managed_accounts.py",
+SQSClient().main_cache_dir_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "..", "..", "..",
+            "ignore",
+            "cache"
+        )
     )
-)
-
-accounts = CommonUtils.load_object_from_module(accounts_file_full_path, "main")
-AWSAccount.set_aws_account(accounts["1111"])
-AWSAccount.set_aws_region(accounts["1111"].regions["us-west-2"])
 
 mock_values_file_path = os.path.abspath(
     os.path.join(
@@ -36,11 +29,14 @@ mock_values_file_path = os.path.abspath(
 )
 mock_values = CommonUtils.load_object_from_module(mock_values_file_path, "main")
 
+# pylint: disable = missing-function-docstring
 
+@pytest.mark.wip
 def test_init_client():
     assert isinstance(SQSClient(), SQSClient)
 
 
+@pytest.mark.skip
 def test_provision_queue():
     client = SQSClient()
     sqs_queue = SQSQueue({})
@@ -56,6 +52,7 @@ def test_provision_queue():
     assert sqs_queue.queue_url is not None
 
 
+@pytest.mark.skip
 def test_provision_queue_dlq():
     client = SQSClient()
     sqs_queue = SQSQueue({})
@@ -71,6 +68,7 @@ def test_provision_queue_dlq():
     assert sqs_queue.queue_url is not None
 
 
+@pytest.mark.skip
 def test_provision_queue_update():
     client = SQSClient()
     sqs_queue = SQSQueue({})
@@ -90,9 +88,38 @@ def test_provision_queue_update():
     client.provision_queue(sqs_queue)
     assert sqs_queue.queue_url is not None
 
+@pytest.mark.wip
+def test_yield_queues_get_tags_true():
+    client = SQSClient()
+    obj = None
+    for obj in client.yield_queues(get_tags=True):
+        break
+    assert obj.arn is not None
+    assert obj.tags is not None
 
-if __name__ == "__main__":
-    test_init_client()
-    test_provision_queue_dlq()
-    test_provision_queue()
-    test_provision_queue_update()
+
+@pytest.mark.wip
+def test_yield_queues_get_tags_false():
+    client = SQSClient()
+    obj = None
+    for obj in client.yield_queues(get_tags=False):
+        break
+    assert obj.arn is not None
+    assert obj.tags is None
+
+
+@pytest.mark.wip
+def test_yield_queues_raw():
+    client = SQSClient()
+    dict_src = None
+    for dict_src in client.yield_queues_raw():
+        break
+    assert dict_src.get("QueueArn") is not None
+    assert dict_src.get("QueueUrl") is not None
+
+
+@pytest.mark.wip
+def test_get_region_queues():
+    client = SQSClient()
+    ret = client.get_region_queues(Region.get_region("us-west-2"))
+    assert len(ret) > 0
