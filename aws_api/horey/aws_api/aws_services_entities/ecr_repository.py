c8a1dcac2a972b5a3d3fce7616af61ecd1f05b07
horey <alexey.beley@gmail.com>
2023-09-27 21:58:18 +0300

    fix get_tags

--- aws_api/horey/aws_api/aws_services_entities/ecr_repository.py
+++ aws_api/horey/aws_api/aws_services_entities/ecr_repository.py
@@ -1,20 +1,19 @@
 """
-AWS Lambda representation
+AWS ECR Repo representation
 """
-import pdb
 
 from horey.aws_api.aws_services_entities.aws_object import AwsObject
-from horey.aws_api.base_entities.region import Region
 
 
 class ECRRepository(AwsObject):
     """
-    AWS VPC class
+    Main class
     """
 
     def __init__(self, dict_src, from_cache=False):
         super().__init__(dict_src)
         self._region = None
+        self.arn = None
 
         if from_cache:
             self._init_object_from_cache(dict_src)
@@ -49,18 +48,30 @@ class ECRRepository(AwsObject):
         self._init_from_cache(dict_src, options)
 
     def generate_create_request(self):
-        request = dict()
-        request["repositoryName"] = self.name
-        request["tags"] = self.tags
+        """
+        Standard.
+
+        :return:
+        """
+        request = {"repositoryName": self.name, "tags": self.tags}
         return request
 
     def generate_dispose_request(self):
-        request = dict()
-        request["repositoryName"] = self.name
-        request["force"] = True
+        """
+        Standard.
+
+        :return:
+        """
+        request = {"repositoryName": self.name, "force": True}
         return request
 
     def update_from_raw_create(self, dict_src):
+        """
+        Standard.
+
+        :param dict_src:
+        :return:
+        """
         init_options = {
             "repositoryArn": lambda x, y: self.init_default_attr(
                 x, y, formatted_name="arn"
@@ -79,20 +90,3 @@ class ECRRepository(AwsObject):
         }
 
         self.init_attrs(dict_src, init_options)
-
-    @property
-    def region(self):
-        if self._region is not None:
-            return self._region
-
-        if self.arn is not None:
-            self._region = Region.get_region(self.arn.split(":")[3])
-
-        return self._region
-
-    @region.setter
-    def region(self, value):
-        if not isinstance(value, Region):
-            raise ValueError(value)
-
-        self._region = value
