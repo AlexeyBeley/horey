5d38603fcc211a29de5be3aedb21b2019ec945f6
horey <alexey.beley@gmail.com>
2024-12-16 12:41:15 -0600

    tests

--- alert_system/horey/alert_system/__init__.py
+++ alert_system/horey/alert_system/__init__.py
@@ -2,4 +2,4 @@
 Logging package. Log formatting and routing.
 """
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
--- lfs/directory_layout_bootstrap.sh
+++ lfs/directory_layout_bootstrap.sh
@@ -16,3 +16,39 @@ esac
 
 
 mkdir -pv $LFS/tools
+
+
+groupadd lfs
+useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+
+
+passwd lfs
+# input password
+
+chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
+case $(uname -m) in
+ x86_64) chown -v lfs $LFS/lib64 ;;
+esac
+
+su - lfs
+cat > ~/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+
+cat > ~/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/usr/bin
+if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+PATH=$LFS/tools/bin:$PATH
+CONFIG_SITE=$LFS/usr/share/config.site
+export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
+EOF
+
+cat >> ~/.bashrc << "EOF"
+export MAKEFLAGS=-j$(nproc)
+EOF
