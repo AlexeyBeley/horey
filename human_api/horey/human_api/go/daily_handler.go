a621cba803f5167546f54b197eaf29ffd6e698f4
horey <alexey.beley@gmail.com>
2025-02-07 06:00:22 -0600

    gofmt -w

--- human_api/horey/human_api/go/daily_handler.go
+++ human_api/horey/human_api/go/daily_handler.go
@@ -2,73 +2,68 @@
 go build -gcflags="all=-N -l" daily_handler.go
 dlv exec daily_handler -- --action daily_json_to_hr --src "/Users/alexey.beley/git/horey/human_api/horey/human_api/go/daily_report_sample.json" --dst "dst_file_path"
 go run . --action daily_json_to_hr --src "/Users/alexey.beley/git/horey/human_api/horey/human_api/go/daily_report_sample.json" --dst "dst_file_path"
-
 */
 package main
 
 import (
-  "flag"
-  "log"
-  "encoding/json"
-  "io/ioutil"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
 )
 
-type WorkerWobjReport struct{
-Parent []string `json:"parent"`
-Child []string `json:"child"`
+type WorkerWobjReport struct {
+	Parent []string `json:"parent"`
+	Child  []string `json:"child"`
 }
 
-
-type WorkerDailyReport struct{
-/*
-
-{worker_id: "str"
-new: [{"parent": (type, id, title), "child": (type, item.id, item.title)}],
-active: [{"parent": (type, id, title), "child": (type, item.id, item.title)}],
-blocked: [{"parent": (type, id, title), "child": (type, item.id, item.title)}],
-closed: [{"parent": (type, id, title), "child": (type, item.id, item.title)}]
-*/
-WorkerID string `json:"worker_id"`
-New []WorkerWobjReport `json:"new"`
-Active []WorkerWobjReport `json:"active"`
-Blocked []WorkerWobjReport `json:"blocked"`
-Closed []WorkerWobjReport `json:"closed"`
-
+type WorkerDailyReport struct {
+	/*
+	   {worker_id: "str"
+	   new: [{"parent": (type, id, title), "child": (type, item.id, item.title)}],
+	   active: [{"parent": (type, id, title), "child": (type, item.id, item.title)}],
+	   blocked: [{"parent": (type, id, title), "child": (type, item.id, item.title)}],
+	   closed: [{"parent": (type, id, title), "child": (type, item.id, item.title)}]
+	*/
+	WorkerID string             `json:"worker_id"`
+	New      []WorkerWobjReport `json:"new"`
+	Active   []WorkerWobjReport `json:"active"`
+	Blocked  []WorkerWobjReport `json:"blocked"`
+	Closed   []WorkerWobjReport `json:"closed"`
 }
-
 
 func main() {
-/*
-daily_handler --action daily_json_to_hr --src --dst
-*/
-  action := flag.String("action", "none", "The action to take")
-  src := flag.String("src", "none", "Source file")
-  dst := flag.String("dst", "none", "Destination file")
+	/*
+	   daily_handler --action daily_json_to_hr --src --dst
+	*/
+	action := flag.String("action", "none", "The action to take")
+	src := flag.String("src", "none", "Source file")
+	dst := flag.String("dst", "none", "Destination file")
 
-  flag.Parse()
+	flag.Parse()
 
-  if *action == "daily_json_to_hr" {
-    flag.Parse()
-    workers_daily, err := ConvertDailyJsonToHR(*src, *dst)
-    if err != nil || len(workers_daily) == 0 {
-      log.Fatal(err, workers_daily)
-    }
- }
+	if *action == "daily_json_to_hr" {
+		flag.Parse()
+		workers_daily, err := ConvertDailyJsonToHR(*src, *dst)
+		if err != nil || len(workers_daily) == 0 {
+			log.Fatal(err, workers_daily)
+		}
+	}
 
 }
 
-func ConvertDailyJsonToHR(src_file_path, dst_file_path string) (reports []WorkerDailyReport, err error){
-  log.Printf("Called with src '%s' and dst '%s'", src_file_path, dst_file_path)
+func ConvertDailyJsonToHR(src_file_path, dst_file_path string) (reports []WorkerDailyReport, err error) {
+	log.Printf("Called with src '%s' and dst '%s'", src_file_path, dst_file_path)
 
-  data, err := ioutil.ReadFile(src_file_path)
-  if err != nil {
-      return nil, err
-    }
+	data, err := ioutil.ReadFile(src_file_path)
+	if err != nil {
+		return nil, err
+	}
 
-  err = json.Unmarshal(data, &reports)
-  if err != nil {
-       return nil, err
-        }
+	err = json.Unmarshal(data, &reports)
+	if err != nil {
+		return nil, err
+	}
 
-  return reports, nil
+	return reports, nil
 }
--- human_api/horey/human_api/go/daily_handler_test.go
+++ human_api/horey/human_api/go/daily_handler_test.go
@@ -4,98 +4,97 @@ dlv exec daily_handler -- --action daily_json_to_hr --src "/Users/alexey.beley/g
 go run . --action daily_json_to_hr --src "/Users/alexey.beley/git/horey/human_api/horey/human_api/go/daily_report_sample.json" --dst "dst_file_path"
 */
 package main
-import (
-        "reflect"
-        "testing"
-)
 
+import (
+	"reflect"
+	"testing"
+)
 
 var test_WorkerDailyReport = WorkerDailyReport{
 	WorkerID: "horey",
-	New: []WorkerWobjReport {{Parent: []string {"1", "2", "3"}, Child: []string{"4","5","6"}}},
-	Active: []WorkerWobjReport {},
-	Blocked: []WorkerWobjReport {},
-	Closed: []WorkerWobjReport {},
+	New:      []WorkerWobjReport{{Parent: []string{"1", "2", "3"}, Child: []string{"4", "5", "6"}}},
+	Active:   []WorkerWobjReport{},
+	Blocked:  []WorkerWobjReport{},
+	Closed:   []WorkerWobjReport{},
 }
 
 func TestInitWorkerDailyReport(t *testing.T) {
-  t.Run("Init test", func(t *testing.T){
-    if ( test_WorkerDailyReport.WorkerID != "horey" ) {
-     t.Errorf("ConvertDailyJsonToHR() = %v, want %v", test_WorkerDailyReport.WorkerID, "WorkerID")
-    }
-  })
+	t.Run("Init test", func(t *testing.T) {
+		if test_WorkerDailyReport.WorkerID != "horey" {
+			t.Errorf("ConvertDailyJsonToHR() = %v, want %v", test_WorkerDailyReport.WorkerID, "WorkerID")
+		}
+	})
 }
 
 var test_WorkerDailyReports = []WorkerDailyReport{
-{WorkerID: "horey",
- New: []WorkerWobjReport{
- {
-  Parent: []string {"UserStory", "1", "test User story"},
-  Child: []string {"Task", "11", "test Task"},
- },
- {
-  Parent: []string {"UserStory", "1", "test User story"},
-  Child: []string {"Task", "12", "test Task 2"},
- },
- },
- Active: []WorkerWobjReport{
- {
-  Parent: []string {"UserStory", "2", "test User story2"},
-  Child: []string {"Task", "22", "test Task 22"},
- },
- },
- Blocked: []WorkerWobjReport{
- {
-  Parent: []string{"UserStory", "2", "test User story2"},
-  Child: []string {"Task", "23", "test Task 23"},
- },
- },
- Closed: []WorkerWobjReport{},
-},
+	{WorkerID: "horey",
+		New: []WorkerWobjReport{
+			{
+				Parent: []string{"UserStory", "1", "test User story"},
+				Child:  []string{"Task", "11", "test Task"},
+			},
+			{
+				Parent: []string{"UserStory", "1", "test User story"},
+				Child:  []string{"Task", "12", "test Task 2"},
+			},
+		},
+		Active: []WorkerWobjReport{
+			{
+				Parent: []string{"UserStory", "2", "test User story2"},
+				Child:  []string{"Task", "22", "test Task 22"},
+			},
+		},
+		Blocked: []WorkerWobjReport{
+			{
+				Parent: []string{"UserStory", "2", "test User story2"},
+				Child:  []string{"Task", "23", "test Task 23"},
+			},
+		},
+		Closed: []WorkerWobjReport{},
+	},
 }
 
 func TestInitListOfWorkerDailyReport(t *testing.T) {
-  t.Run("Init test", func(t *testing.T){
-    if ( len(test_WorkerDailyReports) != 1 ) {
-     t.Errorf("ConvertDailyJsonToHR() = %v, want %v", test_WorkerDailyReport.WorkerID, "WorkerID")
-    }
-  })
+	t.Run("Init test", func(t *testing.T) {
+		if len(test_WorkerDailyReports) != 1 {
+			t.Errorf("ConvertDailyJsonToHR() = %v, want %v", test_WorkerDailyReport.WorkerID, "WorkerID")
+		}
+	})
 }
 
 func TestConvertDailyJsonToHR(t *testing.T) {
-  tests:= []struct {
-                name     string
-                filename string
-                want    []WorkerDailyReport
-                wantErr  bool
-        }{
-                {
-                        name:     "Valid JSON invalid data",
-                        filename: "daily_report_sample.json", // Create this test file
-                        want: []WorkerDailyReport {test_WorkerDailyReport},
-                        wantErr: true,
-                },
-                {
-                        name:     "Valid JSON valid data",
-                        filename: "daily_report_sample.json", // Create this test file
-                        want: test_WorkerDailyReports,
-                        wantErr: false,
-                },
-        }
+	tests := []struct {
+		name     string
+		filename string
+		want     []WorkerDailyReport
+		wantErr  bool
+	}{
+		{
+			name:     "Valid JSON invalid data",
+			filename: "daily_report_sample.json", // Create this test file
+			want:     []WorkerDailyReport{test_WorkerDailyReport},
+			wantErr:  true,
+		},
+		{
+			name:     "Valid JSON valid data",
+			filename: "daily_report_sample.json", // Create this test file
+			want:     test_WorkerDailyReports,
+			wantErr:  false,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ConvertDailyJsonToHR(tt.filename, "dst_file_path")
 
-   for _, tt := range tests {
-     t.Run(tt.name, func(t *testing.T){
-     got, err := ConvertDailyJsonToHR(tt.filename, "dst_file_path")
+			if (err != nil) && !tt.wantErr {
+				t.Errorf("readItemsFromFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 
-     if (err != nil) && !tt.wantErr{
-       t.Errorf("readItemsFromFile() error = %v, wantErr %v", err, tt.wantErr)
-       return
-     }
-
-     if !reflect.DeepEqual(got, tt.want) && !tt.wantErr {
-      t.Errorf("ConvertDailyJsonToHR() = %v, want %v", got, tt.want)
-     }
-     })
-   }
- }
+			if !reflect.DeepEqual(got, tt.want) && !tt.wantErr {
+				t.Errorf("ConvertDailyJsonToHR() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
