dd17984045ee612d0f7829c6f2afadf306c5b761
horey <alexey.beley@gmail.com>
2025-03-14 14:16:17 -0500

    revert Use pip api

--- serverless/horey/serverless/packer/packer.py
+++ serverless/horey/serverless/packer/packer.py
@@ -79,7 +79,6 @@ class Packer:
         @param clean_install:
         @return:
         """
-        raise DeprecationWarning("Use pip api")
         if clean_install:
             if os.path.exists(venv_path):
                 logger.info(f"Removing directory {venv_path}")
