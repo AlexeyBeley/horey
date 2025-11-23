8e2f964f0a3da2c387553868a1d8f2db4689aca6
horey <alexey.beley@gmail.com>
2025-11-23 14:04:24 +0200

    fix venv

--- provision_constructor/horey/provision_constructor/bootstrap.sh
+++ provision_constructor/horey/provision_constructor/bootstrap.sh
@@ -10,6 +10,7 @@ export MAJOR_MINOR_VERSION=${VERSION_NUMBER%.*}
 sudo NEEDRESTART_MODE=a apt install "python${MAJOR_MINOR_VERSION}-venv" -yqq
 #wget https://raw.githubusercontent.com/AlexeyBeley/horey/main/pip_api/horey/pip_api/pip_api_make.py
 
+rm -rf horey
 git clone --branch "cicd_api" https://github.com/AlexeyBeley/horey.git
 cd horey
 ${PYTHON} pip_api/horey/pip_api/pip_api_make.py --install horey.provision_constructor --pip_api_configuration pip_api_configuration.py
