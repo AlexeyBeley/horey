6e6a0ed4d6b7f69c306ab6a11b6579dc53b92c79
horey <alexey.beley@gmail.com>
2025-11-23 13:13:57 +0200

    fix python

--- provision_constructor/horey/provision_constructor/bootstrap.sh
+++ provision_constructor/horey/provision_constructor/bootstrap.sh
@@ -3,10 +3,13 @@ set -ex
 sudo DEBIAN_FRONTEND=noninteractive apt update
 export PYTHON=$(which python >> /dev/null && echo "python" || echo "python3")
 
+export VERSION_STRING=$(${PYTHON} -V)
+export VERSION_NUMBER=${VERSION_STRING#Python }
+export MAJOR_MINOR_VERSION=${VERSION_NUMBER%.*}
 
 sudo NEEDRESTART_MODE=a apt install "python${MAJOR_MINOR_VERSION}-venv" -yqq
 #wget https://raw.githubusercontent.com/AlexeyBeley/horey/main/pip_api/horey/pip_api/pip_api_make.py
 
 git clone --branch "cicd_api" https://github.com/AlexeyBeley/horey.git
 cd horey
-${PYTHON} pip_api/horey/pip_api/pip_api_make.py --install horey.provision_constructor --pip_api_configuration pip_api_configuration.py
+python3 pip_api/horey/pip_api/pip_api_make.py --install horey.provision_constructor --pip_api_configuration pip_api_configuration.py
