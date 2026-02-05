e753f7fdac2bfcbc6356c9eeeb7ea0a34402a692
horey <alexey.beley@gmail.com>
2026-02-05 17:48:02 -0600

    debug

--- provision_constructor/horey/provision_constructor/system_functions/haproxy/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/haproxy/provisioner.py
@@ -7,6 +7,7 @@ import os.path
 import platform
 import json
 
+from horey.common_utils.remoter import Remoter
 from horey.provision_constructor.system_function_factory import SystemFunctionFactory
 
 from horey.provision_constructor.system_functions.system_function_common import (
@@ -22,7 +23,7 @@ BashExecutor.set_logger(logger, override=False)
 @SystemFunctionFactory.register
 class Provisioner(SystemFunctionCommon):
     """
-    Provision ntp service.
+    Provision haproxy service.
     Remove all others.
 
     """
@@ -30,53 +31,26 @@ class Provisioner(SystemFunctionCommon):
     def __init__(self, deployment_dir, force, upgrade, **kwargs):
         super().__init__(deployment_dir, force, upgrade, **kwargs)
 
-    def test_provisioned(self):
-        self.init_apt_packages()
-        return (
-            (not self.apt_check_installed("sntp*"))
-            and (not self.apt_check_installed("chrony*"))
-            and self.check_file_provisioned(
-                "./timesyncd.conf", "/etc/systemd/timesyncd.conf"
-            )
-            and self.check_systemd_service_status(
-                service_name="systemd-timesyncd", min_uptime=20
-            )
-        )
-
-    def _provision(self):
+    def provision_remote(self, remoter: Remoter):
         """
         Provision ntp.
 
         @return:
         """
 
-        self.apt_purge("ntp*")
-        self.apt_purge("sntp*")
-        self.apt_purge("chrony*")
-        release = platform.release()
-        float_release = float(release[:release.find(".", release.find(".")+1)])
+        self.remoter = remoter
+        self.remoter.execute("ls")
 
-        if float_release < 5.15:
-            self.apt_install("systemd-timesyncd")
+        if self.action == "check_backends":
+            return self.check_backends_remote()
+        raise NotImplementedError(self.action)
 
-        try:
-            ret = self.run_bash("sudo timedatectl set-ntp false")
-            logger.info(ret)
-        except BashExecutor.BashError as error_inst:
-            dict_inst = json.loads(str(error_inst))
-            if "NTP not supported" in dict_inst["stderr"]:
-                SystemFunctionFactory.REGISTERED_FUNCTIONS["apt_package_generic"](self.deployment_dir, self.force,
-                                                                   self.upgrade, package_names=["systemd-timesyncd"]).provision()
-                self.run_bash("sudo systemctl restart systemd-timedated")
-                ret = self.run_bash("sudo timedatectl set-ntp false")
-                logger.info(ret)
+    def check_backends_remote(self):
+        """
+        Check backends have live servers
 
-        self.provision_file(
-            "./timesyncd.conf", "/etc/systemd/timesyncd.conf", sudo=True
-        )
-
-        ret = self.run_bash("sudo timedatectl set-ntp true")
-        logger.info(ret)
-
-        ret = self.run_bash("sudo systemctl restart systemd-timedated")
-        logger.info(ret)
+        :return:
+        """
+        breakpoint()
+        ret = self.remoter.execute("echo \"show backend\" | sudo -u haproxy socat stdio unix-connect:/var/run/haproxy/admin.sock")
+        ret = self.remoter.execute("echo \"show servers state {backend_name}\" | sudo -u haproxy socat stdio unix-connect:/var/run/haproxy/admin.sock")
