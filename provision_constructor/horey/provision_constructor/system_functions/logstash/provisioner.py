6e141feae995cae9a1a70ccb1c939ccf1112917b
horey <alexey.beley@gmail.com>
2026-02-06 10:39:27 -0600

    debug

--- provision_constructor/horey/provision_constructor/system_functions/logstash/provisioner.py
+++ provision_constructor/horey/provision_constructor/system_functions/logstash/provisioner.py
@@ -75,34 +75,34 @@ class Provisioner(SystemFunctionCommon):
         :return:
         """
 
+        self.remoter = remoter
         if self.action == "install":
-            return self.provision_remote_install_logstash(remoter)
+            return self.provision_remote_install_logstash()
 
         if self.action == "install_plugin":
-            return self.provision_remote_install_plugin(remoter)
+            return self.provision_remote_install_plugin()
 
         if self.action == "restart":
-            return self.provision_remote_restart(remoter)
+            return self.provision_remote_restart()
 
         raise NotImplementedError(self.action)
 
-    def provision_remote_install_plugin(self, remoter: Remoter):
+    def provision_remote_install_plugin(self):
         """
         Install plugin
 
-        :param remoter:
         :return:
         """
 
         plugin_name = self.kwargs.get("plugin_name")
         if not plugin_name:
             raise ValueError("plugin_name must be specified")
-        remoter.execute(f"sudo /usr/share/logstash/bin/logstash-plugin install {plugin_name}",
+        self.remoter.execute(f"sudo /usr/share/logstash/bin/logstash-plugin install {plugin_name}",
                         self.last_line_validator("Installation successful"))
 
         return True
 
-    def provision_remote_install_logstash(self, remoter: Remoter):
+    def provision_remote_install_logstash(self):
         """
         Install plugin
 
@@ -111,7 +111,6 @@ class Provisioner(SystemFunctionCommon):
                                                                               "openjdk-17-jdk"]).provision_remote(
             remoter)
 
-        :param remoter:
         :return:
         """
 
@@ -119,46 +118,44 @@ class Provisioner(SystemFunctionCommon):
         dst_file_path = "/usr/share/keyrings/elastic-keyring.gpg"
         SystemFunctionFactory.REGISTERED_FUNCTIONS["gpg_key"](self.deployment_dir, self.force,
                                                               self.upgrade, src_url=src_url,
-                                                              dst_file_path=dst_file_path).provision_remote(remoter)
+                                                              dst_file_path=dst_file_path).provision_remote(self.remoter)
 
-        file_names = self.ls_remote(remoter, Path("/etc/apt/sources.list.d/"), sudo=True)
+        file_names = self.ls_remote(Path("/etc/apt/sources.list.d/"), sudo=True)
         elastic_version = "8.x"
         elastic_file_name = f"elastic-{elastic_version}.list"
 
         for file_name in file_names:
             if "elastic-" in file_name:
                 if file_name != elastic_file_name:
-                    self.remove_file_remote(remoter, f"/etc/apt/sources.list.d/{file_name}", sudo=True)
+                    self.remove_file_remote(self.remoter, f"/etc/apt/sources.list.d/{file_name}", sudo=True)
 
         line = f"deb [signed-by={dst_file_path}] https://artifacts.elastic.co/packages/{elastic_version}/apt stable main"
-        self.add_line_to_file_remote(remoter, line=line, file_path=Path(f"/etc/apt/sources.list.d/{elastic_file_name}"),
+        self.add_line_to_file_remote(self.remoter, line=line, file_path=Path(f"/etc/apt/sources.list.d/{elastic_file_name}"),
                                      sudo=True)
 
         SystemFunctionFactory.REGISTERED_FUNCTIONS["apt_package_generic"](self.deployment_dir, True, self.upgrade,
-                                                                          action="update_packages").provision_remote(remoter)
+                                                                          action="update_packages").provision_remote(self.remoter)
 
         SystemFunctionFactory.REGISTERED_FUNCTIONS["apt_package_generic"](self.deployment_dir, self.force, self.upgrade,
                                                                           package_names=[
                                                                               "logstash"]).provision_remote(
-            remoter)
-        remoter.execute("sudo systemctl daemon-reload")
-        remoter.execute("sudo systemctl enable logstash")
+            self.remoter)
+        self.remoter.execute("sudo systemctl daemon-reload")
+        self.remoter.execute("sudo systemctl enable logstash")
         # fix the memory size allowed for logstash
-        remoter.execute("sudo sed -i '/-Xms/c\-Xms2g' /etc/logstash/jvm.options")
-        remoter.execute("sudo sed -i '/-Xmx/c\-Xmx2g' /etc/logstash/jvm.options")
+        self.remoter.execute("sudo sed -i '/-Xms/c\-Xms2g' /etc/logstash/jvm.options")
+        self.remoter.execute("sudo sed -i '/-Xmx/c\-Xmx2g' /etc/logstash/jvm.options")
 
-    @staticmethod
-    def ls_remote(remoter, path, sudo=False):
+    def ls_remote(self, path, sudo=False):
         """
         List remote files
 
-        :param remoter:
         :param path:
         :param sudo:
         :return:
         """
 
-        ret = remoter.execute(f"{'sudo ' if sudo else ''}ls -l {path}")
+        ret = self.remoter.execute(f"{'sudo ' if sudo else ''}ls -l {path}")
         lines = [line.strip("\n") for line in ret[0]]
         ret = []
         for line in reversed(lines):
@@ -167,15 +164,14 @@ class Provisioner(SystemFunctionCommon):
             ret.append(line.split(" ")[-1])
         raise ValueError(f"Was not able to find 'total <number>' in the output: '{ret}'")
 
-    def provision_remote_restart(self, remoter:Remoter):
+    def provision_remote_restart(self):
         """
         Restart logstash service
 
-        :param remoter:
         :return:
         """
 
-        ret = remoter.execute("logstash_pid=$(ps aux | grep 'logstash' | grep 'java' | awk '{print $2}') && echo \"logstash_pid=${logstash_pid}\"")
+        ret = self.remoter.execute("logstash_pid=$(ps aux | grep 'logstash' | grep 'java' | awk '{print $2}') && echo \"logstash_pid=${logstash_pid}\"")
         last_line = ret[0][-1]
 
         if "logstash_pid=" not in last_line:
@@ -183,20 +179,6 @@ class Provisioner(SystemFunctionCommon):
 
         logstash_pid = last_line.strip("\n").split("=")[1]
         if logstash_pid:
-            remoter.execute(f"sudo kill -s 9 {int(logstash_pid)}")
-        else:
-           remoter.execute("sudo systemctl restart logstash")
+            self.remoter.execute(f"sudo kill -s 9 {int(logstash_pid)}")
 
-        self.remoter = remoter
         self.systemctl_restart_service_and_wait_remote("logstash")
-
-        """ 
-        
-        if [ -n "${logstash_pid}" ]
-        then
-              sudo kill -s 9 "${logstash_pid}"
-        else
-              sudo systemctl restart logstash
-        fi
-        sudo systemctl enable logstash
-        """
