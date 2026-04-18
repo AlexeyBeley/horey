b7059bb6916611c2f4bb09f5063ff7712995663e
horey <alexey.beley@gmail.com>
2026-04-18 15:06:42 -0500

    q

--- questrade_api/horey/questrade_api/questrade_api.py
+++ questrade_api/horey/questrade_api/questrade_api.py
@@ -2,7 +2,7 @@
 Shamelessly stolen from:
 https://questrade.com/lukecyca/pyslack
 """
-import datetime
+from datetime import datetime, timezone, timedelta
 import json
 
 import requests
@@ -50,7 +50,6 @@ class QuestradeAPI:
         request = self.create_request(request_path)
 
         headers = {"Authorization": f"Bearer {self.access_token}"}
-        breakpoint()
         response = requests.get(request, headers=headers)
         response.raise_for_status()
         try:
@@ -116,25 +115,27 @@ class QuestradeAPI:
         :return:
         """
 
+        refresh_token = self.configuration.token
+
         response_file_path = self.configuration.data_directory / "response.json"
         if response_file_path.exists():
             with open(response_file_path, encoding="utf-8") as file_handler:
                 response = json.load(file_handler)
-            timestamp_now = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
-            if timestamp_now < response["expires_at"]:
+            timestamp_now = datetime.now(tz=timezone.utc).timestamp()
+            if timestamp_now < response["expires_at"] - 5*60:
                 self.access_token = response["access_token"]
                 self.api_server = response['api_server'].rstrip("/")
                 return
 
             response_file_path.unlink()
+            refresh_token = response["refresh_token"]
 
-        auth_url = f"https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token={self.configuration.token}"
+        auth_url = f"https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token={refresh_token}"
         response = requests.get(auth_url).json()
-        response["expires_at"] = (datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=response["expires_in"])).timestamp()
+        response["expires_at"] = (datetime.now(tz=timezone.utc) + timedelta(seconds=response["expires_in"])).timestamp()
 
         with open(response_file_path, "w", encoding="utf-8") as file_handler:
             json.dump(response, file_handler)
-        breakpoint()
 
         self.access_token = response["access_token"]
         logger.info(f"Connected to Questtrade API, new token: {self.access_token}")
@@ -151,7 +152,6 @@ class QuestradeAPI:
         """
 
         accounts = self.get(f"v1/accounts")
-        breakpoint()
         return accounts
 
     def get_positions(self):
@@ -162,5 +162,131 @@ class QuestradeAPI:
         """
 
         positions = self.get(f"v1/accounts/{self.configuration.account}/positions")
-        breakpoint()
         return positions
+
+    def get_position_history(self, position_id, time_start, time_end, output_file=None):
+        """
+        Get position history.
+
+        :return:
+        """
+
+        start_time = self.convert_time_to_request_format(time_start)
+        end_time = self.convert_time_to_request_format(time_end)
+
+        position_candles = self.get(f"v1/markets/candles/{position_id}?startTime={start_time}&endTime={end_time}&interval=OneMinute")
+        if output_file:
+            with open(self.configuration.data_directory / output_file, "w", encoding="utf-8") as file_handler:
+                json.dump(position_candles, file_handler, indent=2)
+
+        return position_candles
+
+    @staticmethod
+    def convert_time_to_request_format(time_src):
+        """
+        Convert time to request format.
+        :param time_src:
+        :return:
+        """
+
+        formatted = time_src.strftime("%Y-%m-%dT%H:%M:%S%z")
+        # Insert colon in timezone: "2014-10-01T00:00:00-0500" -> "2014-10-01T00:00:00-05:00"
+        return formatted[:-2] + ':' + formatted[-2:]
+
+
+    def get_symbols_raw(self, prefix, offset=None):
+        """
+        Get position history.
+
+        :return:
+        """
+
+        symbols = self.get(f"v1/symbols/search?prefix={prefix}&offset={offset}")
+        symbols = symbols["symbols"]
+        return symbols
+
+    def get_prefix_symbols(self, prefix):
+        """
+        Get position history.
+
+        :return:
+        """
+
+        symbols = []
+        offset = 0
+        while True:
+            logger.info(f"Fetching symbols for prefix: {prefix}" + f" offset: {offset}" if offset else "")
+            symbols_tmp = self.get_symbols_raw(prefix, offset=offset)
+            if not symbols_tmp:
+                break
+            symbols += symbols_tmp
+            offset = len(symbols)
+        with open(self.configuration.data_directory / f"symbols_{prefix}.json", "w", encoding="utf-8") as file_handler:
+            json.dump(symbols, file_handler, indent=2)
+        return symbols
+
+    def get_all_symbols_from_files(self):
+        """
+        Get all symbols from files.
+        :return:
+        """
+
+        files = self.configuration.data_directory.glob("symbols_*.json")
+        symbols = {}
+        for file in files:
+            with open(file, "r", encoding="utf-8") as file_handler:
+                symbols.update({symbol["symbolId"]: symbol for symbol in json.load(file_handler)})
+
+        with open(self.configuration.data_directory / "symbols.json", "w", encoding="utf-8") as file_handler:
+            json.dump(symbols, file_handler, indent=2)
+        logger.info(f"Total symbols: {len(symbols)}")
+
+        return symbols
+
+    def get_tradable_stocks(self):
+        """
+        Get tradable stocks.
+
+        :return:
+        """
+        with open(self.configuration.data_directory / "symbols.json" , encoding="utf-8") as file_handler:
+            symbols = json.load(file_handler)
+
+        # all_types = {symbol["securityType"] for symbol in symbols.values()}
+
+        ret = []
+        for symbol in symbols.values():
+            if not symbol["isTradable"]:
+                continue
+            if symbol["securityType"] != "Stock":
+                continue
+
+            ret.append(symbol)
+
+        return ret
+
+    def get_all_stocks_history(self):
+        """
+        Get all stocks history.
+        :return:
+        """
+        output_file_path = self.configuration.data_directory / "stocks_history.json"
+        now = datetime.now(tz=timezone.utc)
+        stocks = self.get_tradable_stocks()
+        with open(output_file_path, encoding="utf-8") as file_handler:
+            stocks_data = json.load(file_handler)
+        known_symbol_ids = [symbol["symbol"]["symbolId"] for symbol in stocks_data]
+        stocks = [stock for stock in stocks if stock["symbolId"] not in known_symbol_ids]
+
+        for i, stock in enumerate(stocks):
+            logger.info(f"Fetching stock history {i}/{len(stocks)} id:{stock['symbolId']} Symbol:{stock['symbol']}")
+            res = self.get_position_history(stock["symbolId"], now - timedelta(days=2),
+                                          now - timedelta(days=1))
+            res["symbol"] = stock
+            stocks_data.append(res)
+            with open(output_file_path, "w", encoding="utf-8") as file_handler:
+                json.dump(stocks_data, file_handler, indent=2)
+        return True
+
+    def calculate_median(self):
+        breakpoint()
--- questrade_api/tests/test_questrade_api.py
+++ questrade_api/tests/test_questrade_api.py
@@ -81,7 +81,7 @@ def fixture_questrade_api(tests_config):
     yield _questrade_api
 
 
-@pytest.mark.wip
+@pytest.mark.unit
 def test_connect(questrade_api):
     assert questrade_api.connect()
 
@@ -93,4 +93,54 @@ def test_get_accounts(questrade_api):
 
 @pytest.mark.unit
 def test_get_positions(questrade_api):
+    questrade_api.connect()
     assert questrade_api.get_positions()
+
+@pytest.mark.unit
+def test_get_position_history(questrade_api):
+    questrade_api.connect()
+    assert questrade_api.get_position_history("43620897")
+
+@pytest.mark.unit
+def test_get_prefix_symbols(questrade_api):
+    questrade_api.connect()
+    assert questrade_api.get_prefix_symbols("B")
+    assert questrade_api.get_prefix_symbols("C")
+    assert questrade_api.get_prefix_symbols("D")
+    assert questrade_api.get_prefix_symbols("E")
+    assert questrade_api.get_prefix_symbols("F")
+    assert questrade_api.get_prefix_symbols("G")
+    assert questrade_api.get_prefix_symbols("H")
+    assert questrade_api.get_prefix_symbols("I")
+    assert questrade_api.get_prefix_symbols("J")
+    assert questrade_api.get_prefix_symbols("K")
+    assert questrade_api.get_prefix_symbols("L")
+    assert questrade_api.get_prefix_symbols("M")
+    assert questrade_api.get_prefix_symbols("N")
+    assert questrade_api.get_prefix_symbols("O")
+    assert questrade_api.get_prefix_symbols("P")
+    assert questrade_api.get_prefix_symbols("Q")
+    assert questrade_api.get_prefix_symbols("R")
+    assert questrade_api.get_prefix_symbols("S")
+    assert questrade_api.get_prefix_symbols("T")
+    assert questrade_api.get_prefix_symbols("U")
+    assert questrade_api.get_prefix_symbols("V")
+    assert questrade_api.get_prefix_symbols("W")
+    assert questrade_api.get_prefix_symbols("X")
+    assert questrade_api.get_prefix_symbols("Y")
+    assert questrade_api.get_prefix_symbols("Z")
+
+@pytest.mark.unit
+def test_get_all_symbols_from_files(questrade_api):
+    assert questrade_api.get_all_symbols_from_files()
+
+@pytest.mark.unit
+def test_get_tradable_stocks(questrade_api):
+    questrade_api.connect()
+    assert questrade_api.get_tradable_stocks()
+
+
+@pytest.mark.wip
+def test_get_all_stocks_history(questrade_api):
+    questrade_api.connect()
+    assert questrade_api.get_all_stocks_history()
