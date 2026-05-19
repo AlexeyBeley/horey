8c6b5f0098b19b73d53323ccea8df04cbe9ab678
horey <alexey.beley@gmail.com>
2026-05-19 11:26:57 -0500

    debug

--- infrastructure_api/horey/infrastructure_api/ec2_api.py
+++ infrastructure_api/horey/infrastructure_api/ec2_api.py
@@ -189,9 +189,9 @@ class EC2API:
         try:
             ec2_instance = self.get_instance(name=name, update_info=True)
         except RuntimeError:
-            if "found: 0" not in repr(RuntimeError) or not missing_ok:
+            if "found: 0" not in str(RuntimeError) or not missing_ok:
                 raise
-            
+
             return True
 
         return self.environment_api.aws_api.ec2_client.dispose_instance(ec2_instance)
