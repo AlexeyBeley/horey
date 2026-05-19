100af0d6b469c386678c54687ff777f70af1dca9
horey <alexey.beley@gmail.com>
2026-05-19 15:27:56 -0500

    debug

--- questrade_api/tests/test_questrade_api.py
+++ questrade_api/tests/test_questrade_api.py
@@ -177,7 +177,7 @@ def test_provision_db(questrade_api):
     assert questrade_api.provision_db_symbols_table()
     assert questrade_api.provision_db_candles_table()
 
-@pytest.mark.wip
+@pytest.mark.unit
 def test_fetch_symbols_by_max_price(questrade_api):
     assert questrade_api.fetch_symbols_by_price_range(0.01, 2)
 
@@ -245,11 +245,11 @@ def test_generate_profit_review(questrade_api):
 
     assert questrade_api.generate_profit_review(time_start, time_end)
 
-@pytest.mark.wip
+@pytest.mark.unit
 def test_update_cheap_candles_with_today_data(questrade_api):
     assert questrade_api.update_cheap_candles_with_today_data()
 
-@pytest.mark.wip
+@pytest.mark.unit
 def test_make_purchase_plan(questrade_api):
     assert questrade_api.make_purchase_plan()
 
