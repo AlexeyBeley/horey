411af811199792e1298c4fdc2b519b85180037ee
horey <alexey.beley@gmail.com>
2026-05-21 14:22:14 -0500

    added async

--- aws_api/horey/aws_api/aws_clients/ec2_client.py
+++ aws_api/horey/aws_api/aws_clients/ec2_client.py
@@ -3077,11 +3077,10 @@ class EC2Client(Boto3Client):
             self.clear_cache(EC2Instance)
             return response
 
-    def start_instances(self, instances, asynchronous=False):
+    def start_instances(self, instances):
         """
         Start instances and wait.
 
-        :param asynchronous:
         :param instances:
         :return:
         """
@@ -3096,9 +3095,6 @@ class EC2Client(Boto3Client):
         for region, ec2_instances in instance_by_region.items():
             self.start_instances_raw(region, request={"InstanceIds": [ec2_instance.id for ec2_instance in ec2_instances]})
 
-        if asynchronous:
-            return True
-
         for ec2_instance in instances:
             self.wait_for_status(
                 ec2_instance,
@@ -3107,8 +3103,6 @@ class EC2Client(Boto3Client):
                 [ec2_instance.State.PENDING],
                 [ec2_instance.State.SHUTTING_DOWN, ec2_instance.State.TERMINATED, ec2_instance.State.STOPPING],
             )
-        time.sleep(60)
-        return True
 
     def start_instances_raw(self, region, request):
         """
--- infrastructure_api/horey/infrastructure_api/ec2_api.py
+++ infrastructure_api/horey/infrastructure_api/ec2_api.py
@@ -161,11 +161,10 @@ class EC2API:
         self.environment_api.aws_api.provision_ec2_instance(ec2_instance)
         return ec2_instance
 
-    def start_instance(self, name=None, asynchronous=True):
+    def start_instance(self, name=None):
         """
         Stop EC2 instance.
 
-        :param asynchronous:
         :param name:
         :return:
         """
@@ -176,10 +175,9 @@ class EC2API:
         if not ec2_instance:
             raise ValueError(f"Was not able to find instance by {name=}")
 
-        if ec2_instance.is_alive():
-            return True
+        if not ec2_instance.is_alive():
+            self.environment_api.aws_api.ec2_client.start_instances([ec2_instance])
 
-        self.environment_api.aws_api.ec2_client.start_instances([ec2_instance], asynchronous=asynchronous)
         return ec2_instance
 
 
