dde4f03d8737a4a419188df876064f691dc1bd4f
horey <alexey.beley@gmail.com>
2026-06-05 15:08:16 -0500

    Debug

--- aws_api/horey/aws_api/aws_clients/ec2_client.py
+++ aws_api/horey/aws_api/aws_clients/ec2_client.py
@@ -3204,7 +3204,7 @@ class EC2Client(Boto3Client):
                     instance_type
                 ]
             },
-        ],
+        ]
 
         return list(self.execute(
             self.get_session_client(region=region).describe_instance_type_offerings, "InstanceTypeOfferings", filters_req=filters_req
