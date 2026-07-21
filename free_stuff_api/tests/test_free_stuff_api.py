9489265cca3876302b0d21e52368d888cc71ac89
horey <alexey.beley@gmail.com>
2026-07-21 17:43:13 -0500

    debug

--- free_stuff_api/tests/test_free_stuff_api.py
+++ free_stuff_api/tests/test_free_stuff_api.py
@@ -10,12 +10,12 @@ from horey.common_utils.free_item import FreeItem
 from horey.free_stuff_api.platform import Platform
 
 config = FreeStuffAPIConfigurationPolicy()
-config.configuration_file_full_path = Path(__file__).parent.parent.parent.parent / "ignore" / "test_frs_api_configuration.py"
-config.horey_directory_path =  Path(__file__).parent.parent.parent
+config.configuration_file_full_path = Path(
+    __file__).parent.parent.parent.parent / "ignore" / "test_frs_api_configuration.py"
+config.horey_directory_path = Path(__file__).parent.parent.parent
 config.init_from_file()
 config.db_file_path = Path("/opt/hfrs/hfrs.sqlite")
 config.region = "us-west-2"
-
 
 
 # pylint: disable= missing-function-docstring
@@ -25,6 +25,7 @@ def fixture_free_stuff_mac_raw():
     free_stuff_api = FreeStuffAPI(config)
     yield free_stuff_api
     free_stuff_api.selenium_api.disconnect()
+
 
 @pytest.fixture(scope="module", name="linux_amd_docker")
 def fixture_linux_amd_docker():
@@ -40,7 +41,7 @@ def fixture_linux_arm_docker():
     config.chrome_path = Path("/opt/chrome/chrome")
     yield free_stuff_api
     free_stuff_api.selenium_api.disconnect()
-    
+
 
 @pytest.fixture(scope="module", name="eks_free_stuff_api")
 def fixture_eks_free_stuff_api():
@@ -49,6 +50,7 @@ def fixture_eks_free_stuff_api():
     config.chrome_path = Path("/opt/chrome/chrome")
     yield free_stuff_api
     free_stuff_api.selenium_api.disconnect()
+
 
 @pytest.mark.unit
 def test_main():
@@ -60,17 +62,25 @@ def test_main():
 def test_add_free_item_to_db():
     free_stuff_api = FreeStuffAPI(config)
     free_item = FreeItem("name", "url")
-    assert free_stuff_api.add_free_item_to_db(free_item)
+    assert free_stuff_api.add_free_item_to_db(Platform("test", "test") ,free_item)
+
 
 @pytest.mark.unit
 def test_notify_about_new_item():
-
     free_stuff_api = FreeStuffAPI(config)
-    url = 'https://www.facebook.com/marketplace/item/951081683937401/?ref=category_feed&referral_code=null&referral_story_type=listing&tracking=%7B%22qid%22%3A%22-7208799824235551155%22%2C%22mf_story_key%22%3A%2226591833053762219%22%2C%22commerce_rank_obj%22%3A%22%7B%5C%22target_id%5C%22%3A26591833053762219%2C%5C%22target_type%5C%22%3A0%2C%5C%22primary_position%5C%22%3A36%2C%5C%22ranking_signature%5C%22%3A5258348999073026090%2C%5C%22ranking_request_id%5C%22%3A907753257306275608%2C%5C%22commerce_channel%5C%22%3A504%2C%5C%22value%5C%22%3A0.010212191960216%2C%5C%22candidate_retrieval_source_map%5C%22%3A%7B%5C%2226591833053762219%5C%22%3A3001%7D%7D%22%7D&__tn__=!%3AD'
-    image_url = 'https://scontent-ord5-2.xx.fbcdn.net/v/t39.84726-6/655689552_2096376911202768_5175015685651605039_n.jpg?stp=c0.87.526.526a_dst-jpg_p526x395_tt6&_nc_cat=103&ccb=1-7&_nc_sid=92e707&_nc_ohc=ohuQ_BIylTsQ7kNvwE9l7n4&_nc_oc=Adp85U_yKavgCJ8TR5m_8I_jgBGXxKD41x0i08zHAW6fA0gwl0WA7K4HP2UwmrOfsyI&_nc_zt=14&_nc_ht=scontent-ord5-2.xx&_nc_gid=Upe7bcFcJ5AzRRVLGeyEOQ&_nc_ss=7a30f&oh=00_AfwHui_aKVKaDF6THy2XB4Kr9oj70oOUdXlSkDgcufoIeg&oe=69C5136E'
+    url = ('https://www.facebook.com/marketplace/item/951081683937401/?ref=category_feed&referral_code=null&referral_story_type=listing&tracking=%7B%22qid%22%3A%22-'
+           '7208799824235551155%22%2C%22mf_story_key%22%3A%2226591833053762219%22%2C%22commerce_rank_obj%22%3A%22%7B%5C%22target_id%5C%22%3A26591833053762219%2C%5C%'
+           '22target_type%5C%22%3A0%2C%5C%22primary_position%5C%22%3A36%2C%5C%22ranking_signature%5C%22%3A5258348999073026090%2C%5C%22ranking_request_id%5C%22%3A9077'
+           '53257306275608%2C%5C%22commerce_channel%5C%22%3A504%2C%5C%22value%5C%22%3A0.010212191960216%2C%5C%22candidate_retrieval_source_map%5C%22%3A%7B%5C%22265918'
+           '33053762219%5C%22%3A3001%7D%7D%22%7D&__tn__=!%3AD')
+
+    image_url = ('https://scontent-ord5-2.xx.fbcdn.net/v/t39.84726-6/655689552_2096376911202768_5175015685651605039_n.jpg?stp=c0.87.526.526a_dst-jpg_p526x395_tt6&_'
+                 'nc_cat=103&ccb=1-7&_nc_sid=92e707&_nc_ohc=ohuQ_BIylTsQ7kNvwE9l7n4&_nc_oc=Adp85U_yKavgCJ8TR5m_8I_jgBGXxKD41x0i08zHAW6fA0gwl0WA7K4HP2UwmrOfsyI&_'
+                 'nc_zt=14&_nc_ht=scontent-ord5-2.xx&_nc_gid=Upe7bcFcJ5AzRRVLGeyEOQ&_nc_ss=7a30f&oh=00_AfwHui_aKVKaDF6THy2XB4Kr9oj70oOUdXlSkDgcufoIeg&oe=69C5136E')
     description = "test description"
     free_item = FreeItem("test", url, image_url=image_url, description=description)
     assert free_stuff_api.notify_about_new_item(free_item)
+
 
 @pytest.mark.unit
 def test_provision_infra():
@@ -88,17 +98,21 @@ def test_update():
 def test_provision_db(free_stuff_mac_raw):
     assert free_stuff_mac_raw.provision_db()
 
+
 @pytest.mark.unit
 def test_add_platform(free_stuff_mac_raw):
-    assert free_stuff_mac_raw.add_platform(Platform(None,"Facebook"))
+    assert free_stuff_mac_raw.add_platform(Platform(None, "Facebook"))
+
 
 @pytest.mark.unit
 def test_delete_platform_items(free_stuff_mac_raw):
     assert free_stuff_mac_raw.delete_platform_items(free_stuff_mac_raw.platforms[0])
 
+
 @pytest.mark.unit
 def test_main_free_stuff_linux_amd_docker(linux_amd_docker):
     assert linux_amd_docker.main()
+
 
 @pytest.mark.unit
 def test_main_free_stuff_linux_arm_docker(linux_arm_docker):
@@ -109,10 +123,12 @@ def test_main_free_stuff_linux_arm_docker(linux_arm_docker):
 def test_provision_eks_infra(eks_free_stuff_api):
     assert eks_free_stuff_api.provision_eks_infra()
 
-@pytest.mark.wip
+
+@pytest.mark.unit
 def test_main_free_stuff_mac_raw(free_stuff_mac_raw):
     assert free_stuff_mac_raw.main()
 
-@pytest.mark.unit
+
+@pytest.mark.wip
 def test_update_component(eks_free_stuff_api):
     assert eks_free_stuff_api.update_component()
