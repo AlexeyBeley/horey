d25e56df9d17b2c6fb17089728d186e146ec0357
horey <alexey.beley@gmail.com>
2026-08-13 14:38:54 -0500

    Debug

--- infrastructure_api/horey/infrastructure_api/db_api.py
+++ infrastructure_api/horey/infrastructure_api/db_api.py
@@ -524,7 +524,6 @@ class DBAPI:
                 "Key": "name",
                 "Value": user_group_name
             })
-        breakpoint()
 
         try:
             default_user = self.get_elasticache_user(region, "default-secure")
