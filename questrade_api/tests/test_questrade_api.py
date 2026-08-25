97b4073559604d2d803550d11c9bf909916b363c
horey <alexey.beley@gmail.com>
2026-08-24 19:01:41 -0500

    debug

--- questrade_api/tests/test_questrade_api.py
+++ questrade_api/tests/test_questrade_api.py
@@ -241,7 +241,7 @@ def test_generate_profit_review(questrade_api):
     assert questrade_api.generate_profit_review(time_start, time_end)
     breakpoint()
 
-@pytest.mark.unit
+@pytest.mark.wip
 def test_fetch_symbols_by_max_price(questrade_api):
     assert questrade_api.fetch_symbols_by_price_range(0.001, 2)
 
