64774024fa3e3f3e39f9ff518f092a928f15511c
horey <alexey.beley@gmail.com>
2026-09-03 14:35:39 -0500

    Debug

---
+++ alert_system/horey/alert_system/elasticache/serverless_alert_builder.py
@@ -0,0 +1,98 @@
+"""
+Monitor mysql like a boss!
+"""
+import json
+from horey.common_utils.common_utils import CommonUtils
+from horey.aws_api.aws_services_entities.rds_db_cluster import RDSDBCluster
+from horey.aws_api.aws_services_entities.cloud_watch_alarm import CloudWatchAlarm
+from horey.h_logger import get_logger
+from statistics import median, mean
+
+logger = get_logger()
+
+
+class ServerlessAlertBuilder:
+    """
+    Provision
+    """
+
+    # pylint: disable = too-many-arguments
+    def __init__(self, cache):
+        self.cache = cache
+
+    def generate_metric_filters(self):
+        """
+        Metrics used to monitor the cluster.
+
+        :return:
+        """
+
+        ret = []
+
+        dimensions = [
+            {"Name": "clusterId", "Value": self.cache.name},
+        ]
+
+        ret.append(
+            {"Namespace": "AWS/ElastiCache",
+             "Dimensions": dimensions})
+
+        return ret
+
+    def generate_metric_alarm_limits(self, metric, statistics_data):
+        """
+        Generate alarm value min and max.
+
+        :param statistics_data:
+        :param metric_raw:
+        :return:
+        """
+        
+        min_multiplier = 0.01
+        max_multiplier = 10.0
+
+        median_max = median(x["Maximum"] for x in statistics_data)
+        mean_max = mean(x["Maximum"] for x in statistics_data)
+        absolute_max_value = max(x["Maximum"] for x in statistics_data)
+
+        median_min = median(x["Minimum"] for x in statistics_data)
+        mean_min = mean(x["Minimum"] for x in statistics_data)
+        absolute_min_value = min(x["Minimum"] for x in statistics_data)
+
+        median_average = median(x["Average"] for x in statistics_data)
+        mean_average = mean(x["Average"] for x in statistics_data)
+        absolute_min_average = min(x["Average"] for x in statistics_data)
+        
+        match metric.name:
+            case "NewConnections":
+                return absolute_min_value, absolute_max_value*max_multiplier
+            case _:
+                logger.info(f"{absolute_min_value=}, {absolute_max_value=}, {metric.name=}, {median_min=}, {mean_min=}, {median_max=}, {mean_max=}, {median_average=}, {mean_average=}")
+                breakpoint()
+                return absolute_min_value, absolute_max_value
+
+    def generate_metric_alarm_slug(self, metric):
+        """
+        camel_case_to_snake_case
+
+        :param metric_raw:
+        :return:
+        """
+
+        dimension_names_to_values = {x["Name"]: x["Value"] for x in metric.dimensions}
+        prefix = ""
+
+        for key in ["clusterId"]:
+            if key in dimension_names_to_values:
+                prefix += dimension_names_to_values[key] + "_"
+                del dimension_names_to_values[key]
+
+        if dimension_names_to_values:
+            raise NotImplementedError(f"Not all dimensions handled: {metric.dimensions=}, {dimension_names_to_values=}")
+
+        if not prefix:
+            raise NotImplementedError(f"Can not generate unique slug for metric: {metric.dict_src}")
+
+        snake_case = CommonUtils.camel_case_to_snake_case(metric.name)
+
+        return prefix + snake_case 
